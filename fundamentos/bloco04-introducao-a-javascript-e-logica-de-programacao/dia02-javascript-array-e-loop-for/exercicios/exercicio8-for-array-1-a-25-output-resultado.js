cd7bdf11071b7a9335398f17e62717c0caffc2e0
find_CarolinaCosta <carolinadacosta1997@gmail.com>
2021-07-29 22:08:18 -0300

    Adiciona exercicios do dia 2

---
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia02-javascript-array-e-loop-for/exercicios/exercicio8-for-array-1-a-25-output-resultado.js
@@ -0,0 +1,5 @@
+let count = [];
+for (let index = 0; index <= 25; index += 1) {
+    count.push(index);
+}
+console.log(count);
---
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia02-javascript-array-e-loop-for/exercicios/exercicio9-output-divisao-dos-elementos-po-2.js
@@ -0,0 +1,9 @@
+let count = [];
+let countDivision = 0;
+for (let index = 0; index <= 25; index += 1) {
+    count.push(index);
+}
+
+for (let i = 0; i < count.length; i += 1) {
+    console.log(count[i] / 2);
+}
