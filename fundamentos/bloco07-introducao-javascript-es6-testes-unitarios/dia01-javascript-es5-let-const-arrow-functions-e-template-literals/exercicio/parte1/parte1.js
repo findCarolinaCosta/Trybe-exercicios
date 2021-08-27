6a6ffad9ae55f8f51ec2607f84ef3544855518e3
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-08-27 19:13:19 -0300

    Resolve exercicio parte1 - bloco 7, dia 1.

--- fundamentos/bloco07-introducao-javascript-es6-testes-unitarios/dia01-javascript-es5-let-const-arrow-functions-e-template-literals/exercicio/parte1/parte1.js
+++ fundamentos/bloco07-introducao-javascript-es6-testes-unitarios/dia01-javascript-es5-let-const-arrow-functions-e-template-literals/exercicio/parte1/parte1.js
@@ -1,22 +1,20 @@
-function testingScope(escopo) {
-  if (escopo === true) {
-    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-    console.log(ifScope);
-  } else {
-    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
-  }
-  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-}
-
-testingScope(true);
-
-
 // 1 - Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
 // * Modifique a estrutura da função para que ela seja uma arrow function .
 // * Modifique as concatenações para template literals.
 // * Copie o código abaixo.
+const testingScope = escopo => {
+
+  if (escopo) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+
+testingScope(true);
 
 // 2 - Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
 // * Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
@@ -26,5 +24,23 @@ testingScope(true);
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // Seu código aqui.
+const sortArray = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7;
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
 
-console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+  return oddsAndEvens;
+}
+
+console.log(`Escrito da maneira dificil e levando em consideração somente um array especifico: ${sortArray()}`); // será necessário alterar essa linha 😉
+
+const sortArray2 = array => {
+  const sortArray = array.sort((num1, num2) => num1 - num2);
+  return sortArray;
+}
+console.log(`Escrito de forma dinâmica que serviria para outros arrays: ${sortArray2(oddsAndEvens)}`);
+
+console.log(`Código escrito em uma linha, dinâmico: ${oddsAndEvens.sort((num1, num2) => num1 - num2)}`);
