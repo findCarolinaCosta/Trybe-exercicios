ec82ed6c47552333c70c946132aad77d6c529050
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-08-30 20:13:51 -0300

    Resolve exercicio parte 1, 7.2

--- fundamentos/bloco07-introducao-javascript-es6-testes-unitarios/dia02-javascript-es6-objects/exercicios/parte2.js
+++ fundamentos/bloco07-introducao-javascript-es6-testes-unitarios/dia02-javascript-es6-objects/exercicios/parte2.js
@@ -18,12 +18,27 @@ const lesson3 = {
   turno: 'noite',
 };
 
-// 1 - Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
-// 2 - Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
-// 3 - Crie uma função para mostrar o tamanho de um objeto.
-// 4 - Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
-// 5 - Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+// 1
+//add chave que permanece como forma de atualização
+const addChaveTurno = (obj, chave, turno) => obj[chave] = turno;
+console.log(addChaveTurno(lesson2, 'turno', 'manhã'));
+console.log(lesson2);
 
+// 2
+const keys = (obj) => Object.keys(obj);
+console.log(keys(lesson1));
+
+// 3
+const size = (obj) => Object.keys(obj).length;
+console.log(size(lesson1));
+
+// 4
+const values = (obj) => Object.values(obj);
+console.log(values(lesson1));
+
+
+// 5
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
 console.log(allLessons);
 /*
 {
@@ -45,14 +60,34 @@ console.log(allLessons);
 };
 */
 
-// 8 - Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
-// 7 - Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
 
+// 6
+const numberStudents = (obj) => {
+  let total = 0;
+  const array = Object.keys(obj);
+  for (index in array) {
+    total += obj[array[index]].numeroEstudantes;
+  }
+  return total;
+}
+console.log(numberStudents(allLessons));
+
+
+// 7
+const getValueByNumber = (obj, number) => Object.values(obj)[number];
 console.log(getValueByNumber(lesson1, 0));
 // Output: 'Matématica'
 
-// 8 - Crie uma função que verifique se o par(chave / valor) existe na função.Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave.Exemplo:
+// 8
 
+const verifyPair = (obj, chave, valor) => {
+  const array2 = Object.entries(obj);
+  let verify = false;
+  for (let index in array2) {
+    if (array2[index][0] === chave && array2[index][1] === valor) verify = true;
+  }
+  return verify;
+};
 console.log(verifyPair(lesson3, 'turno', 'noite'));
 // Output: true,
 console.log(verifyPair(lesson3, 'materia', 'Maria Clara'));
