7991cee5792538f264f1b0056af7bdf24d4ba93d
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-08-31 19:33:48 -0300

    Adiciona solucao segundo requisito parte 1, (7.3)

--- fundamentos/bloco07-introducao-javascript-es6-testes-unitarios/dia03-testes-unitarios-em-javascript/ex2-praticando-a-implementação-de-testes -parte1.js
+++ fundamentos/bloco07-introducao-javascript-es6-testes-unitarios/dia03-testes-unitarios-em-javascript/ex2-praticando-a-implementação-de-testes -parte1.js
@@ -11,3 +11,16 @@ function myRemove(arr, item) {
 }
 
 // implemente seus testes aqui
+// 1
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4], 'Não é para aparecer o 3');
+
+// 2
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4], 'não retorna o array');
+
+//3
+const myList = [5, 6, 7, 8];
+myRemove(myList, 5);
+assert.deepStrictEqual(myList, [5, 6, 7, 8]);
+
+//4
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
