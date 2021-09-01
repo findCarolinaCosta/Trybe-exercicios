d6dd196833722506448f2dbb597a3a76e356e293
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-08-31 21:31:24 -0300

    Adiciona solucao do segundo exercicio parte 2, (7.3)

--- fundamentos/bloco07-introducao-javascript-es6-testes-unitarios/dia03-testes-unitarios-em-javascript/Parte 2 - Praticando TDD/ex2-parte2-function-word-lengths.js
+++ fundamentos/bloco07-introducao-javascript-es6-testes-unitarios/dia03-testes-unitarios-em-javascript/Parte 2 - Praticando TDD/ex2-parte2-function-word-lengths.js
@@ -1,6 +1,12 @@
 const assert = require('assert');
-// escreva a função wordLengths aqui
-// 2 - Escreva a função wordLengths para passar nos testes já implementados.
+
+const wordLengths = (arrays) => {
+  const result = [];
+  for (let index = 0; index < arrays.length; index += 1) {
+    result.push(arrays[index].length);
+  }
+  return result;
+}
 
 const words = ['sun', 'potato', 'roundabout', 'pizza'];
 const expected = [3, 6, 10, 5];
