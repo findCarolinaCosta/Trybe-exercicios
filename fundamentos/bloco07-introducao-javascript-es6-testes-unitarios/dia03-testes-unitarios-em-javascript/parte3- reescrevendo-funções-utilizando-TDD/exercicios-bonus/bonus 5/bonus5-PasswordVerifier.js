8703004baab891e30598ab53c780ee5d090f4a98
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-05 18:03:55 -0300

    (7.3) Passo 4 - Verifica que a senha deve ter pelo menos uma letra maiuscula, bonus 5

--- fundamentos/bloco07-introducao-javascript-es6-testes-unitarios/dia03-testes-unitarios-em-javascript/parte3- reescrevendo-funções-utilizando-TDD/exercicios-bonus/bonus 5/bonus5-PasswordVerifier.js
+++ fundamentos/bloco07-introducao-javascript-es6-testes-unitarios/dia03-testes-unitarios-em-javascript/parte3- reescrevendo-funções-utilizando-TDD/exercicios-bonus/bonus 5/bonus5-PasswordVerifier.js
@@ -1,6 +1,7 @@
 function verify(password) {
   if (password === null) return false;
   else if (password.length <= 8) return false;
+  else if (!/[A-Z0-9]/.test(password)) return false;
   return true;
 }
 
--- fundamentos/bloco07-introducao-javascript-es6-testes-unitarios/dia03-testes-unitarios-em-javascript/parte3- reescrevendo-funções-utilizando-TDD/exercicios-bonus/bonus 5/bonus5.testes.js
+++ fundamentos/bloco07-introducao-javascript-es6-testes-unitarios/dia03-testes-unitarios-em-javascript/parte3- reescrevendo-funções-utilizando-TDD/exercicios-bonus/bonus 5/bonus5.testes.js
@@ -6,3 +6,4 @@ assert.strictEqual(typeof verify, 'function');
 assert.strictEqual(typeof verify, 'function'); // Password should be larger than 8 chars
 assert(!verify('12345678'));
 assert(!verify(null)); // Password should not be null
+assert(!verify('gandalfthegray')); // Password should have one uppercase letter at least
