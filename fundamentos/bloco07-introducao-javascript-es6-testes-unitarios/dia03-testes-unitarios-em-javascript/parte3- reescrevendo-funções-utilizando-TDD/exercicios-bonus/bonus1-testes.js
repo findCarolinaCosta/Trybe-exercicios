60db52a1df1c0109625e10002fcba0568e8219e0
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-04 22:26:42 -0300

    (7.3)Refatora e "linka"  testes no arquivo principal da funcao bonus 1

--- fundamentos/bloco07-introducao-javascript-es6-testes-unitarios/dia03-testes-unitarios-em-javascript/parte3- reescrevendo-funções-utilizando-TDD/exercicios-bonus/bonus1-testes.js
+++ fundamentos/bloco07-introducao-javascript-es6-testes-unitarios/dia03-testes-unitarios-em-javascript/parte3- reescrevendo-funções-utilizando-TDD/exercicios-bonus/bonus1-testes.js
@@ -1,18 +1,19 @@
 const assert = require('assert');
+const getChange = require('./bonus1-vending-machine');
 
-let result = getChange(1, 1); // no change/coins just an empty array
+let result = getChange(1, 1);
 let expected = [];
 assert.deepStrictEqual(result, expected);
 
-result = getChange(215, 300); // expect an array containing [50, 20, 10, 5]
+result = getChange(215, 300);
 expected = [50, 20, 10, 5];
 assert.deepStrictEqual(result, expected);
 
-result = getChange(486, 600); // expect an array containing [100, 10, 2, 2]
+result = getChange(486, 600);
 expected = [100, 10, 2, 2];
 assert.deepStrictEqual(result, expected);
 
-result = getChange(12, 400); // expect an array containing [200, 100, 50, 20, 10, 5, 2, 1]
+result = getChange(12, 400);
 expected = [200, 100, 50, 20, 10, 5, 2, 1];
 assert.deepStrictEqual(result, expected);
 
