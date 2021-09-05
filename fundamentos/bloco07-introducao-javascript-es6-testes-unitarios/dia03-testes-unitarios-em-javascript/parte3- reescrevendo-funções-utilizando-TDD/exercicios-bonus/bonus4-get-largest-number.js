5f8fe17c684900bd8e74ddaa92751634d287db26
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-05 17:14:41 -0300

    (7.3) Refatora funcao de acordo com o que e esperado nos teste

--- fundamentos/bloco07-introducao-javascript-es6-testes-unitarios/dia03-testes-unitarios-em-javascript/parte3- reescrevendo-funções-utilizando-TDD/exercicios-bonus/bonus4-get-largest-number.js
+++ fundamentos/bloco07-introducao-javascript-es6-testes-unitarios/dia03-testes-unitarios-em-javascript/parte3- reescrevendo-funções-utilizando-TDD/exercicios-bonus/bonus4-get-largest-number.js
@@ -1,14 +1,6 @@
 const getLargestNumber = (array) => {
-  let largestNumber;
-  for (let index = 0; index < array.length - 1; index += 1) {
-    if (array[index] > array[index + 1]) {
-      largestNumber = [array[index]];
-    }
-  }
-  return largestNumber;
+  let largestNumber = array.slice().sort((a, b) => b - a);
+  return largestNumber[0];
 }
-
-const parameter = [45, 8, 2, 50, 1, 7, 99];
-const result = 99;
 
 module.exports = getLargestNumber;
