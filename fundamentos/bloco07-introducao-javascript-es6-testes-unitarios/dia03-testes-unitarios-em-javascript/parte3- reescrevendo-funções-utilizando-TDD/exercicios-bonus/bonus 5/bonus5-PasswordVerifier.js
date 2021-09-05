ddb39e3878983193eb17c99fb7cb2f6f2314b5e4
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-05 18:39:19 -0300

    (7.3) Declara retornos da funcao em forma de mensagem descrevendo procedimentos, bonus 5

--- fundamentos/bloco07-introducao-javascript-es6-testes-unitarios/dia03-testes-unitarios-em-javascript/parte3- reescrevendo-funções-utilizando-TDD/exercicios-bonus/bonus 5/bonus5-PasswordVerifier.js
+++ fundamentos/bloco07-introducao-javascript-es6-testes-unitarios/dia03-testes-unitarios-em-javascript/parte3- reescrevendo-funções-utilizando-TDD/exercicios-bonus/bonus 5/bonus5-PasswordVerifier.js
@@ -1,9 +1,12 @@
+const passwordOK = 'A senha está Ok';
+const needLc = 'A senha deve ter pelo menos uma letra minúscula e pelo menos um número';
+
 function verify(password) {
   if (password === null) return false;
   else if (password.length <= 8) return false;
   else if (!/[A-Z]/.test(password) || !/[a-z]/.test(password) || !/[0-9]/.test(password))
-    return false;
-  return true;
+    return needLc;
+  return passwordOK;
 }
 
 module.exports = verify;
