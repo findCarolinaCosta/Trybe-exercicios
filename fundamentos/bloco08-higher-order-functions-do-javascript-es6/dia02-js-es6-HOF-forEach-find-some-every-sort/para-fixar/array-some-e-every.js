609d780c19e47e47804cebfc0e5790473d30ba68
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-06 18:51:55 -0300

    (8.2) Resolve requisito 1 exercicio para fixar Array.some

--- fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia02-js-es6-HOF-forEach-find-some-every-sort/para-fixar/array-some-e-every.js
+++ fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia02-js-es6-HOF-forEach-find-some-every-sort/para-fixar/array-some-e-every.js
@@ -3,7 +3,7 @@
 const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
 
 const hasName = (arr, name) => {
-  //Adicione seu código aqui
+  return arr.some((currentName) => currentName === name);
 }
 
 console.log(hasName(names, 'Ana'));
