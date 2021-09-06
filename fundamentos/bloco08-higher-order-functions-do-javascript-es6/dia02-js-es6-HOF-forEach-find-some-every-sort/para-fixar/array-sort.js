ddd637113acf7807214201ac6501e6b7805c694a
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-06 19:02:15 -0300

    (8.2) Resolve requisito 2 do exercicio para fixar Array.sort

--- fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia02-js-es6-HOF-forEach-find-some-every-sort/para-fixar/array-sort.js
+++ fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia02-js-es6-HOF-forEach-find-some-every-sort/para-fixar/array-sort.js
@@ -13,3 +13,15 @@ people.sort((personA, personB) => personA.age - personB.age);
 console.log(people);
 
 //2 - Modifique o sort do exercício anterior para que ordene o array pela idade das pessoas em ordem decrescente .
+const peopleCopy = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+// Adicione se código aqui
+peopleCopy.sort((personA, personB) => personB.age - personA.age);
+
+console.log(peopleCopy);
