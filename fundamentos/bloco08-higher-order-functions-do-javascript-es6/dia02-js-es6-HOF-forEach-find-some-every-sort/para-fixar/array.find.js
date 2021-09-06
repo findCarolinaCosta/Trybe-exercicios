54de6c7b4779f548d7b3f9046d0f43808439a6eb
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-06 18:45:55 -0300

    (8.2) Resolve requisito 2 exercicio para fixar Array.find

--- fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia02-js-es6-HOF-forEach-find-some-every-sort/para-fixar/array.find.js
+++ fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia02-js-es6-HOF-forEach-find-some-every-sort/para-fixar/array.find.js
@@ -14,6 +14,7 @@ const names = ['João', 'Irene', 'Fernando', 'Maria'];
 
 const findNameWithFiveLetters = () => {
   // Adicione seu código aqui:
+  return names.find((name) => name.length === 5);
 }
 
 console.log(findNameWithFiveLetters());
