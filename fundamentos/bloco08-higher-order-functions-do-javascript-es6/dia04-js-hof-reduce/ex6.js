df25cad9f74ddf61d953da5be368d30d06a2a65c
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-11 20:03:05 -0300

    (8.4) Resolve exercicio 6

--- fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia04-js-hof-reduce/ex6.js
+++ fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia04-js-hof-reduce/ex6.js
@@ -6,7 +6,12 @@ const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
 function studentAverage() {
-  // escreva seu código aqui
+  const average = students.map((name, index) => ({
+    name,
+    average: grades[index].reduce((acumulator, current) => acumulator + current)
+      / grades[index].length,
+  }));
+  return average;
 }
 
 const expected = [
