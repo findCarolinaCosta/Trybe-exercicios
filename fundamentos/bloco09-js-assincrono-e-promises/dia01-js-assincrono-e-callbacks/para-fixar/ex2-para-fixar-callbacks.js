ed21cc8b1941ab797702a4c5d6bfe82c1e0313e1
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-15 18:48:44 -0300

    (9.1) Resolve exercicio para fixar 2 - Callbacks

--- fundamentos/bloco09-js-assincrono-e-promises/dia01-js-assincrono-e-callbacks/para-fixar/ex2-para-fixar-callbacks.js
+++ fundamentos/bloco09-js-assincrono-e-promises/dia01-js-assincrono-e-callbacks/para-fixar/ex2-para-fixar-callbacks.js
@@ -3,14 +3,14 @@ const userNationality = ({ firstName, nationality }) => `${firstName} is ${natio
 
 const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
 
-const getUser = () => {
+const getUser = callback => {
   setTimeout(() => {
     const user = {
       firstName: "Ivan",
       lastName: "Ivanovich",
-      nationality: "Russian",
+      nationality: "Russian"
     };
-    console.log(user);
+    console.log(callback(user));
   }, delay());
 };
 
@@ -18,3 +18,4 @@ getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depoi
 getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
 
 // 2 - No código abaixo você tem a função getUser modificada, que agora funciona de modo assíncrono e imprime dados de uma pessoa qualquer depois de um certo tempo. Complete a função getUser de forma que ela receba um callback para que possa realizar as operações abaixo sobre a pessoa.
+// Obs: note e averigue o comportamento assíncrono da função getUser ao chamar getUser(userFullName) seguido de getUser(userNationality) . Tem hora que é impresso antes no console o nome da pessoa, e tem hora que é impressa antes a nacionalidade da pessoa!
