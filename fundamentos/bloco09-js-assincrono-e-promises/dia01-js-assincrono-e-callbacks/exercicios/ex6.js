4a681da2af829f44015273343735aa38dd28921d
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-16 18:38:50 -0300

    (9.1) Resolve exericicio 6 para praticar

--- fundamentos/bloco09-js-assincrono-e-promises/dia01-js-assincrono-e-callbacks/exercicios/ex6.js
+++ fundamentos/bloco09-js-assincrono-e-promises/dia01-js-assincrono-e-callbacks/exercicios/ex6.js
@@ -5,7 +5,7 @@ const messageDelay = () => Math.floor(Math.random() * 5000);
 const getMarsTemperature = () => {
   const maxTemperature = 58;
   return Math.floor(Math.random() * maxTemperature);
-}
+};
 
 const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9 / 5) + 32;
 
@@ -19,9 +19,14 @@ const handleError = (errorReason) =>
   console.log(`Error getting temperature: ${errorReason}`);
 
 // definição da função sendMarsTemperature...
+const sendMarsTemperature = (pass, err) => {
+  const currentTemperature = getMarsTemperature();
+  const messageSuccessfullySent = Math.random() <= 0.6;
+  setTimeout(() => {
+    if (messageSuccessfullySent) pass(currentTemperature)
+    else err('Data failure');
+  }, messageDelay());
+}
 
-// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
 sendMarsTemperature(temperatureInFahrenheit, handleError);
-
-// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
 sendMarsTemperature(greet, handleError);
