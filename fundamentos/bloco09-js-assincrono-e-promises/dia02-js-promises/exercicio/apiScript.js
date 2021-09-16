47f41d460eb7fee943ff12a04ae330d5b4a4c648
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-16 20:27:44 -0300

    (9.2)  Requisições a API usando fetch

--- fundamentos/bloco09-js-assincrono-e-promises/dia02-js-promises/exercicio/apiScript.js
+++ fundamentos/bloco09-js-assincrono-e-promises/dia02-js-promises/exercicio/apiScript.js
@@ -1,8 +1,13 @@
-// apiScript.js
+// apiScript.js     
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject);
 };
 
 window.onload = () => fetchJoke();
