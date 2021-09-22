f01fcc4a0e10996ed7e8b7c8bd5e60faa01f3f79
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-22 16:10:11 -0300

    (10.2) Para fixar - implementa uma função que espera um tempo e retorna a soma de dois números

--- fundamentos/bloco10-testes-automatizados-com-jest/dia02-jest-testes-assincronos/para-fixar/callbacks/testando-codigos-assincronos.test.js
+++ fundamentos/bloco10-testes-automatizados-com-jest/dia02-jest-testes-assincronos/para-fixar/callbacks/testando-codigos-assincronos.test.js
@@ -1,11 +1,19 @@
-test('Não deveria passar!', (done) => {
+const asyncSum = (a, b, callback) => {
   setTimeout(() => {
+    const result = a + b;
+    callback(result);
+  }, 500);
+};
+
+test('Testando asyncSum, soma 5 mais 10', (done) => {
+  asyncSum(5, 10, (result) => {
     try {
-      expect(10).toBe(5);
-      console.log('Deveria falhar!');
+      expect(result).toBe(15);
       done();
     } catch (error) {
-      done(error); // Alteramos esta linha
+      done(error);
     }
-  }, 500);
+  });
 });
+
+//Quando estiver realizando testes, é muito importante verificar se os resultados exibidos não são falso-positivos. No exemplo acima, em que o teste está passando, experimente mudar a implementação da função asyncSum para que retorne valores incorretos e verifique se o teste irá falhar.
