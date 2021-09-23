8076b341bbc964b3cbde0200104d57d8523ec2e7
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-23 16:19:05 -0300

    (10.3) Testa se função retorna valor que defini (explicação comentada)

--- fundamentos/bloco10-testes-automatizados-com-jest/dia03-jest-simulando-comportamentos/para-fixar/mockando-funções-testes/service.test.js
+++ fundamentos/bloco10-testes-automatizados-com-jest/dia03-jest-simulando-comportamentos/para-fixar/mockando-funções-testes/service.test.js
@@ -8,4 +8,13 @@ test("#randomRgbColor", () => {
   expect(service.randomRgbColor).toHaveBeenCalled();
 });
 
-//Ao declarar service.randomRgbColor = jest.fn(); , estamos dizendo ao teste que, a partir daquele momento, estamos tomando controle da função service.randomRgbColor e que ela será uma simulação da função original.
+test("#randomRgbColor2", () => {
+  // testando se a função foi chamada e qual seu retorno
+  service.randomRgbColor = jest.fn().mockReturnValue("rgb(255, 255, 255)");
+
+  service.randomRgbColor();
+  expect(service.randomRgbColor).toHaveBeenCalled();
+  expect(service.randomRgbColor()).toBe("rgb(255, 255, 255)");
+}); // "No contexto deste teste, quando essa função for chamada, ela retornará o valor que eu defini, ao invés de um valor aleatório!"
+
+// Na terceira linha do exemplo acima, estamos manualmente chamando a função service.randomRgbColor(); . Caso isso não seja feito, o teste expect(service.randomRgbColor).toHaveBeenCalled() irá falhar. Isso acontece porque mockar uma função redefine seu comportamento, mas não a executa. A propriedade toHaveBeenCalled() espera que a função dentro do expect tenha sido executada por alguma chamada anterior a essa linha dentro do contexto desse teste.
