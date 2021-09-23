2e82bfa313762ea1e15eda02fc898e0a687fbcbd
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-23 18:36:21 -0300

    (10.3) Refatorando teste usando jest.spyOn()

--- fundamentos/bloco10-testes-automatizados-com-jest/dia03-jest-simulando-comportamentos/para-fixar/Trabalhando com mock e funções originais/math2.test.js
+++ fundamentos/bloco10-testes-automatizados-com-jest/dia03-jest-simulando-comportamentos/para-fixar/Trabalhando com mock e funções originais/math2.test.js
@@ -1,26 +1,25 @@
 const math = require('../mockando-modulos/math');
 
 test("#somar", () => {
-  // testando a implementação original, o mock e o mock resetado
+  // testando a implementação original, o mock e a restauração da função original
 
   // implementação original
   expect(math.somar(1, 2)).resolves.toBe(3);
 
   // criando o mock e substituindo a implementação para uma subtração
-  math.somar = jest.fn().mockImplementation((a, b) => a - b);
+  const mockSomar = jest
+    .spyOn(math, "somar")
+    .mockImplementation((a, b) => a - b);
 
   math.somar(5, 1);
-  expect(math.somar).toHaveBeenCalledTimes(1);
-  expect(math.somar(5, 1)).toBe(4);
-  expect(math.somar).toHaveBeenCalledTimes(2);
-  expect(math.somar).toHaveBeenLastCalledWith(5, 1);
+  expect(mockSomar).toHaveBeenCalledTimes(1);
+  expect(mockSomar(5, 1)).toBe(4);
+  expect(mockSomar).toHaveBeenCalledTimes(2);
+  expect(mockSomar).toHaveBeenLastCalledWith(5, 1);
 
-  // resetando o mock
-  math.somar.mockReset();
-  expect(math.somar(1, 2)).toBe(undefined);
-  expect(math.somar).toHaveBeenCalledTimes(1);
-  expect(math.somar).toHaveBeenLastCalledWith(1, 2);
+  // restaurando a implementação original
+  math.somar.mockRestore();
+  expect(math.somar(1, 2)).resolves.toBe(3);
 });
 
-//passa porém com obs: Jest não saiu um segundo após a conclusão do teste.
-// Isso geralmente significa que há operações assíncronas que não foram interrompidas em seus testes. Considere executar Jest com `--detectOpenHandles` para solucionar esse problema.
+//No exemplo anterior, por termos usado o jest.fn(), não há como restaurar as implementações originais da função, pois suas funcionalidades não permitem. A única ferramenta que nos permite transitar entre simulação e comportamento original é o jest.spyOn() .
