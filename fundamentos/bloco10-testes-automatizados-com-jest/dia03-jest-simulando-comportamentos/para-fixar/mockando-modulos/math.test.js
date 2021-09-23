4033ba6d57c98754a3b2a8312dc33232b6fe708a
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-23 18:07:01 -0300

    (10.3) Adiciona teste com jest.mock de função subtração definida sob controle e infos comentadas

--- fundamentos/bloco10-testes-automatizados-com-jest/dia03-jest-simulando-comportamentos/para-fixar/mockando-modulos/math.test.js
+++ fundamentos/bloco10-testes-automatizados-com-jest/dia03-jest-simulando-comportamentos/para-fixar/mockando-modulos/math.test.js
@@ -4,11 +4,21 @@ jest.mock("./math"); // com jest.mock para simular o comportamento de todas as f
 test("#somar", () => {
   // Aqui testamos se função foi chamada, quantas vezes foi chamada, quais parâmetros foram usados e qual seu retorno
 
-  math.somar.mockImplementation((a, b) => a + b);
+  math.somar.mockImplementation((a, b) => a + b); //constrole adição
   math.somar(1, 2);
 
   expect(math.somar).toHaveBeenCalled();
   expect(math.somar).toHaveBeenCalledTimes(1);
   expect(math.somar).toHaveBeenCalledWith(1, 2);
   expect(math.somar(1, 2)).toBe(3);
+
+
+  math.subtrair.mockImplementation((a, b) => a - b); //controle subtração
+  math.subtrair(3, 2);
+  expect(math.subtrair).toHaveBeenCalled();
+  expect(math.subtrair).toHaveBeenCalledTimes(1);
+  expect(math.subtrair).toHaveBeenCalledWith(3, 2);
+  expect(math.subtrair(3, 2)).toBe(1);
 });
+
+// pode-se ter o controle e assim "modificar" para agir como é esperado, e com jest.mock(linha 2) e obtendo o controle com .mockImplementation por exemplo se tem uma simulação do resultado.
