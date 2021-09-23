5a548a0aa2de26d0512b0f904acc2c9696847f81
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-23 16:12:22 -0300

    (10.3) Adiciona comentário explicação do erro

--- fundamentos/bloco10-testes-automatizados-com-jest/dia03-jest-simulando-comportamentos/para-fixar/mockando-funções-testes/service.test.js
+++ fundamentos/bloco10-testes-automatizados-com-jest/dia03-jest-simulando-comportamentos/para-fixar/mockando-funções-testes/service.test.js
@@ -7,3 +7,5 @@ test("#randomRgbColor", () => {
 });
 
 // Erro de correspondência: o valor recebido deve ser uma função simulada ou espiã
+
+// Esse erro acontece porque a propriedade toHaveBeenCalled , assim como outras que serão ensinadas a seguir, são exclusivas para funções simuladas. Ou seja: se você não simula uma função, a propriedade não funciona corretamente.
