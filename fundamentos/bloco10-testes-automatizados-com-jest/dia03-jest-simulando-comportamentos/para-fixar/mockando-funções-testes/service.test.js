f6f0b68279fab0684cb3c8d37bdbac8eeca463dd
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-23 16:09:54 -0300

    (10.3) Adiciona função mas não passa no teste (explicação comentada)

--- fundamentos/bloco10-testes-automatizados-com-jest/dia03-jest-simulando-comportamentos/para-fixar/mockando-funções-testes/service.test.js
+++ fundamentos/bloco10-testes-automatizados-com-jest/dia03-jest-simulando-comportamentos/para-fixar/mockando-funções-testes/service.test.js
@@ -1,5 +1,9 @@
+const service = require('./service');
+
 test("#randomRgbColor", () => {
   // testando se a função foi chamada
   service.randomRgbColor();
   expect(service.randomRgbColor).toHaveBeenCalled();
 });
+
+// Erro de correspondência: o valor recebido deve ser uma função simulada ou espiã
