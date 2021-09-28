47144f0c207458182b633f14470d5dfca08bc05d
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-27 23:14:34 -0300

    (10.1) Ex3 fizzbuzz - cola função e exporta para o teste passando em 100%

--- fundamentos/bloco10-testes-automatizados-com-jest/dia01-primeiros-passos-no-jest/exercicios/parte-1/refazer-exericios-do-bloco-7-dia-3-de-testes-unitarios-em-js/parte-1/ex3-fizbuzz.test.js
+++ fundamentos/bloco10-testes-automatizados-com-jest/dia01-primeiros-passos-no-jest/exercicios/parte-1/refazer-exericios-do-bloco-7-dia-3-de-testes-unitarios-em-js/parte-1/ex3-fizbuzz.test.js
@@ -1,3 +1,5 @@
+const myFizzBuzz = require('./ex3-fizbuzz');
+
 describe('A função myFizzBuzz(num) recebe um número num e retorna "fizzbuzz" se o número for divisível por 3 e 5 , retorna "fizz" se for divisível apenas por 3 , retorna "buzz" se divisível apenas por 5 , retorna o próprio número caso não seja divisível por 3 ou 5 e retorna false caso num não seja um número', () => {
   it('1. Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
     expect(myFizzBuzz(30)).toBe('fizzbuzz');
