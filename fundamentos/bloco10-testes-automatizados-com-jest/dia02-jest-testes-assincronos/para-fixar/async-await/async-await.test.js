a361314251f8f19a45fb19747d09047e4a64ca3e
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-30 18:23:43 -0300

    (10.2) Para fixar - Usando o mesmo código anterior mas com pequenas mudanças

---
+++ fundamentos/bloco10-testes-automatizados-com-jest/dia02-jest-testes-assincronos/para-fixar/async-await/async-await.test.js
@@ -0,0 +1,7 @@
+describe('Para fixar Async/Await', () => {
+  test('Testando com async/await', async () => {
+    const listDogs = await findAnimalsByType('Dog');
+    expect(listDogs[0].name).toEqual('Dorminhoco');
+    expect(listDogs[1].name).toEqual('Soneca');
+  });
+})
