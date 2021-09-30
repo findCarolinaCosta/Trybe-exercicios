90ba45880a4c89478a9c32affa12bbc8d7a576dc
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-30 17:19:43 -0300

    (10.2) Para fixar - Converte para função assíncrona o teste

--- fundamentos/bloco10-testes-automatizados-com-jest/dia02-jest-testes-assincronos/para-fixar/Promises/testando-promises.test.js
+++ fundamentos/bloco10-testes-automatizados-com-jest/dia02-jest-testes-assincronos/para-fixar/Promises/testando-promises.test.js
@@ -34,10 +34,9 @@ describe('Quando o tipo do animal existe', () => {
 });
 
 describe('Quando o tipo do animal, não existe', () => {
-  test('Retorne a lista de animais', () => {
-    expect.assertions(1);
-    return findAnimalsByType('Lion').catch((error) => (
+  test('Retorne a lista de animais', async () => {
+    await findAnimalsByType('Lion').catch((error) => (
       expect(error.message).toMatch('Não possui esse tipo de animal.')
     ));
   });
-}); // teste que faz asserções, garantimos que todas elas foram executadas, e caso não sejam, o teste falha por outro motivo (quantidade de asserções).
+}); // Converte para função assíncrona, não precisando mais do assertions
