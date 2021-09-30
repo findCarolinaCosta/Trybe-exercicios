5c97a40c68042794a1d1419cdea37b8d705c371d
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-30 18:34:37 -0300

    (10.2) Para fixar adiciona assertions, async/await para que o teste falhe, deixando de ser falso-positivo então

--- fundamentos/bloco10-testes-automatizados-com-jest/dia02-jest-testes-assincronos/para-fixar/async-await/async-await.test.js
+++ fundamentos/bloco10-testes-automatizados-com-jest/dia02-jest-testes-assincronos/para-fixar/async-await/async-await.test.js
@@ -6,12 +6,12 @@ describe('Para fixar Async/Await', () => {
     expect(listDogs[0].name).toEqual('Dorminhoco');
     expect(listDogs[1].name).toEqual('Soneca');
   });
-  // Abaixo está o código para quando ocorre o reject da promise. É necessário adicionar o bloco try/catch .
   test('Testando com async/await, testando o reject', async () => {
+    expect.assertions(1);
     try {
       await findAnimalsByType('Lion');
     } catch (error) {
       expect(error).toEqual(new Error('Não possui esse tipo de animal.'));
-    } //getListAnimals não lançar o erro como esperamos o bloco do catch não será executado, fazendo com que a nossa asserção não tenha efeito e o teste seja um falso-positivo
-  });
+    }
+  }); //Da mesma forma que usamos expect.assertions com promises também precisamos usar com o async/await , para garantir que caso a asserção não seja executada o teste falhe.
 })
