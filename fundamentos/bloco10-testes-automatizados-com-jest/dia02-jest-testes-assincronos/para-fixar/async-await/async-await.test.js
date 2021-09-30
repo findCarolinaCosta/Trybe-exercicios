e92893786e8d45c6bd4796dfa817a58320352e31
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-30 18:29:47 -0300

    (10.2) Para fixar - para pegar o erro com try/catch porém resulta em falso-positivo

--- fundamentos/bloco10-testes-automatizados-com-jest/dia02-jest-testes-assincronos/para-fixar/async-await/async-await.test.js
+++ fundamentos/bloco10-testes-automatizados-com-jest/dia02-jest-testes-assincronos/para-fixar/async-await/async-await.test.js
@@ -1,8 +1,17 @@
+const findAnimalsByType = require('../Promises/testando-promises.test')
+
 describe('Para fixar Async/Await', () => {
   test('Testando com async/await', async () => {
     const listDogs = await findAnimalsByType('Dog');
     expect(listDogs[0].name).toEqual('Dorminhoco');
     expect(listDogs[1].name).toEqual('Soneca');
   });
+  // Abaixo está o código para quando ocorre o reject da promise. É necessário adicionar o bloco try/catch .
+  test('Testando com async/await, testando o reject', async () => {
+    try {
+      await findAnimalsByType('Lion');
+    } catch (error) {
+      expect(error).toEqual(new Error('Não possui esse tipo de animal.'));
+    } //getListAnimals não lançar o erro como esperamos o bloco do catch não será executado, fazendo com que a nossa asserção não tenha efeito e o teste seja um falso-positivo
+  });
 })
-//A diferença entre elas é o async . Como existe uma promise a ser testada, é necessário o uso do await , para que o teste espere a finalização da promise e, em seguida, execute o teste.
