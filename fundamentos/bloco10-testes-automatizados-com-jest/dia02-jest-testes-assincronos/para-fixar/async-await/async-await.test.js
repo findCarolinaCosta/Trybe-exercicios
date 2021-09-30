4564529fef8c5cba4c53217d5973be8021cfbe9b
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-30 18:37:36 -0300

    (10.2) Para fixar - async/await com .resolves/.rejects, exemplo

--- fundamentos/bloco10-testes-automatizados-com-jest/dia02-jest-testes-assincronos/para-fixar/async-await/async-await.test.js
+++ fundamentos/bloco10-testes-automatizados-com-jest/dia02-jest-testes-assincronos/para-fixar/async-await/async-await.test.js
@@ -13,5 +13,26 @@ describe('Para fixar Async/Await', () => {
     } catch (error) {
       expect(error).toEqual(new Error('Não possui esse tipo de animal.'));
     }
-  }); //Da mesma forma que usamos expect.assertions com promises também precisamos usar com o async/await , para garantir que caso a asserção não seja executada o teste falhe.
-})
+  });
+});
+
+//Também é possivel combinar async/await com .resolves/.rejects :
+describe('Testando Async/Await - findAnimalsByType', () => {
+  describe('Quando o tipo do animal existe', () => {
+    test('Retorne a lista de animais', async () => {
+      const listDogs = [
+        { name: 'Dorminhoco', age: 1, type: 'Dog' },
+        { name: 'Soneca', age: 2, type: 'Dog' },
+      ];
+      await expect(findAnimalsByType('Dog')).resolves.toEqual(listDogs);
+    });
+  });
+
+  describe('Quando o tipo de animal não existe', () => {
+    test('Retorna um erro', async () => {
+      expect.assertions(1);
+      await expect(findAnimalsByType('Lion')).rejects
+        .toEqual(new Error('Não possui esse tipo de animal.'));
+    });
+  });
+});
