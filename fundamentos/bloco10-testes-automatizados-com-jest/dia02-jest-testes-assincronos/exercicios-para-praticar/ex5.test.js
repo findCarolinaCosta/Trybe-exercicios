2552f58e8c3f6a3434ed666141c8611953ba1a52
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-10-01 16:09:28 -0300

    (10.2) Ex5 - adiciona obs comentadas do fluxo real do Setup e Teardown

--- fundamentos/bloco10-testes-automatizados-com-jest/dia02-jest-testes-assincronos/exercicios-para-praticar/ex5.test.js
+++ fundamentos/bloco10-testes-automatizados-com-jest/dia02-jest-testes-assincronos/exercicios-para-praticar/ex5.test.js
@@ -12,3 +12,45 @@ describe('5 - Para este exercício, tente adivinhar a saída dos console.log dos
     test('Imagino que primeiro o log do beforeEach depois o log desse teste 2 e só', () => console.log('2 - test'));
   });
 })
+
+//Bom jeito de fixar, não sabia que os Setup e Teardown funcionaria antes e depois do teste seguinte também, fiquei com dúvida por causa que estava dentro de um outro describe. Mas de qualquer forma identifica como um próximo teste
+
+// console.log
+//     1 - beforeEach (acertei)
+
+//       at Object.<anonymous> (ex5.test.js:3:28)
+
+//   console.log
+//     1 - test (acertei)
+
+//       at Object.<anonymous> (ex5.test.js:6:257)
+
+//   console.log
+//     1 - afterEach  (acertei)
+
+//       at Object.<anonymous> (ex5.test.js:4:27)
+
+//   console.log
+//     1 - beforeEach  (Não sabia rs)
+
+//       at Object.<anonymous> (ex5.test.js:3:28)
+
+//   console.log
+//     2 - beforeEach (acertei)
+
+//       at Object.<anonymous> (ex5.test.js:9:30)
+
+//   console.log
+//     2 - test  (acertei)
+
+//       at Object.<anonymous> (ex5.test.js:12:100)
+
+//   console.log
+//     2 - afterEach  (acertei)
+
+//       at Object.<anonymous> (ex5.test.js:10:29)
+
+//   console.log
+//     1 - afterEach  (Não sabia, foi um bom aprendizado)
+
+//       at Object.<anonymous> (ex5.test.js:4:27)
