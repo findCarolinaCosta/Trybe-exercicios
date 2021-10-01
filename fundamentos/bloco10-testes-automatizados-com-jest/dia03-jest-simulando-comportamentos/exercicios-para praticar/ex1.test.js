37a5c4d1345a0cdcf8dcbfafc9852ec03d75bf35
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-10-01 19:29:55 -0300

    (10.3)Ex 1 -  Adiciona função de gerar números aleatórios e exporta pata arquivo de teste

--- fundamentos/bloco10-testes-automatizados-com-jest/dia03-jest-simulando-comportamentos/exercicios-para praticar/ex1.test.js
+++ fundamentos/bloco10-testes-automatizados-com-jest/dia03-jest-simulando-comportamentos/exercicios-para praticar/ex1.test.js
@@ -1,3 +1,5 @@
+const service = require('./ex1');
+
 describe('1. Crie uma função que gere um número aleatório entre 0 e 100. Você irá criar também os testes para essa função. Utilizando o mock, defina o retorno padrão como 10. Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada.', () => {
   describe('Testa função de gerar números aleatórios', () => {
     service.retornaNumeroAleatorio = jest.fn().mockReturnValue(10);
