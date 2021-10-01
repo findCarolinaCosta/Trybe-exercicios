e1ea4537202a6742155eeddf1434f1c062a45119
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-10-01 18:53:25 -0300

    (10.3) Adiciona requisito 5 do ex. para fixar

--- fundamentos/bloco10-testes-automatizados-com-jest/dia03-jest-simulando-comportamentos/para-fixar/para-fixar.test.js
+++ fundamentos/bloco10-testes-automatizados-com-jest/dia03-jest-simulando-comportamentos/para-fixar/para-fixar.test.js
@@ -49,6 +49,21 @@ describe('', () => {
   });
 
   it('5. Faça o mock da função subtrair de maneira que seja possível restaurar sua implementação original. Defina como retorno padrão o valor "20". Teste o número de chamadas e o retorno. Restaure a implementação original da função e teste sua execução.', () => {
+    const mockSubtrair = jest.spyOn(math, "subtrair");
+    mockSubtrair.mockImplementation((a, b) => a * b);
+    mockSubtrair.mockReturnValue(20);
 
+    expect(mockSubtrair(10, 2)).toBe(20);
+    expect(mockSubtrair).toHaveBeenCalled();
+    expect(mockSubtrair).toHaveBeenCalledTimes(2);
+    expect(mockSubtrair).toHaveBeenCalledWith(10, 2);
+
+    mockSubtrair.mockRestore();
+    mockSubtrair.mockReturnValue(8);
+
+    expect(mockSubtrair(10, 2)).toBe(8);
+    expect(mockSubtrair).toHaveBeenCalled();
+    expect(mockSubtrair).toHaveBeenCalledTimes(1);
+    expect(mockSubtrair).toHaveBeenCalledWith(10, 2);
   });
 });
