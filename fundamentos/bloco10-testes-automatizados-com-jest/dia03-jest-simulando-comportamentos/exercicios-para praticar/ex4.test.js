96c64c49aaf15fe6f7758f48a95f13b8c81cf3e6
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-10-01 20:24:13 -0300

    (10.3) Ex4 - Adiciona função e exporta para mockar função, para receber um parâmetro e retornar em caixa baixa

--- fundamentos/bloco10-testes-automatizados-com-jest/dia03-jest-simulando-comportamentos/exercicios-para praticar/ex4.test.js
+++ fundamentos/bloco10-testes-automatizados-com-jest/dia03-jest-simulando-comportamentos/exercicios-para praticar/ex4.test.js
@@ -1,3 +1,6 @@
+const service = require("./ex4");
+jest.mock("./ex4");
+
 describe('4. Dentro de um mesmo arquivo, crie três funções. A primeira deve receber uma string e retorná-la em caixa alta. A segunda deve também receber uma string e retornar só a primeira letra. A terceira deve receber duas strings e concatená-las. Faça o mock do arquivo. Faça uma nova implementação para a primeira função, mas agora ela deve retornar a string em caixa baixa. Para a segunda função, uma nova implementação deve retornar a última letra de uma string. A terceira deve receber três strings e concatená-las.', () => {
   describe('Testa função de string em caixa alta', () => {
     service.firstFunction.mockImplementation(a => a.toLowerCase());
