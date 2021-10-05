ba33d5926e5c29599ae65639b423e3ecb2ed22db
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-10-04 21:23:42 -0300

    (11.1) Ex2- Adiciona lista de tarefas

---
+++ 02-desenvolvimento-front-end/bloco-11-introducao-a-react/dia-01-hello-world-no-react/exercicios/todo-list/src/todo-list.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+const Task = (value) => {
+  return (
+    <li key={value}>{value}</li>
+  );
+}
+
+
+const tasksList = ['Estudar', 'Limpar a casa', 'Limpar cantinho das gatas', 'Tomar banho', 'Almoçar'];
+
+class GetList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Todo List:</h1>
+        <ul>{tasksList.map(taskItem => Task(taskItem))}</ul>
+      </div>
+    );
+  }
+}
+
+
+
+export { GetList };
