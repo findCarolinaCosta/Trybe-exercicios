63076294fb6c29920da6ff96e03e19e36f8da62c
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-10-07 15:36:03 -0300

    (11.1) Requisito 1 de fixação da introdução aos eventos

--- 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formulario-com-react/dia-01-componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
+++ 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formulario-com-react/dia-01-componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+// Para fixar
+// 1 - Crie uma aplicação React com npx create-react-app fancy-buttons . Altere o componente App.js para que seja um componente de classe e contenha um botão associado a um evento que imprime um texto qualquer via console.log() . Não precisa se preocupar com a sintaxe correta para funções de eventos por enquanto!!
+
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* Embora isso funcione, essa DEFINITIVAMENTE
+não é a maneira correta de se criar eventos
+em React! A função se refere ao componente,
+então deve ser parte de sua classe! */
+function handleClick() {
+  console.log('Clicou no botão!')
+}
+
+class App extends Component {
+  /* Repare que, diferentemente do HTML, no
+  JSX você associa uma função a um evento
+  passando a própria função entre chaves `{}` */
+  render() {
+    return <button onClick={handleClick}>Meu botão</button>
+  }
 }
 
 export default App;
