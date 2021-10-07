f39f67d718dac8d9ac1cc375a4a0ea556b177016
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-10-07 15:48:28 -0300

    (11.1) Requisito 3 de fixação da vinculando funções à classe

--- 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formulario-com-react/dia-01-componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
+++ 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formulario-com-react/dia-01-componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
@@ -1,15 +1,12 @@
 // Para fixar
-// 2 - Faça com que sua aplicação exiba três botões lado a lado com textos diferentes. Cada botão clicado deve acionar um evento diferente, cada um escrevendo algo diferente no console do navegador via console.log() .
+// 3 - Declare dentro da classe do seu componente dos exercícios de fixação acima a função que lida com o evento que antes era lidado por uma função do lado de fora da classe!
+//Não são reconhecidas ainda
 
 import React, { Component } from 'react';
 import './App.css';
 
-/* Embora isso funcione, essa DEFINITIVAMENTE
-não é a maneira correta de se criar eventos
-em React! A função se refere ao componente,
-então deve ser parte de sua classe! */
-
-function handleBtnOne() {
+class App extends Component {
+  function handleBtnOne() {
   console.log('Clicou no botão 1!')
 }
 
@@ -21,19 +18,15 @@ function handleBtnThree() {
   console.log('Clicou no botão 3!')
 }
 
-class App extends Component {
-  /* Repare que, diferentemente do HTML, no
-  JSX você associa uma função a um evento
-  passando a própria função entre chaves `{}` */
-  render() {
-    return (
-      <div>
-        <button onClick={handleBtnOne}>Meu botão 1</button>
-        <button onClick={handleBtnTwo}>Meu botão 2</button>
-        <button onClick={handleBtnThree}>Meu botão 3</button>
-      </div>
-    );
-  }
+render() {
+  return (
+    <div>
+      <button onClick={handleBtnOne}>Meu botão 1</button>
+      <button onClick={handleBtnTwo}>Meu botão 2</button>
+      <button onClick={handleBtnThree}>Meu botão 3</button>
+    </div>
+  );
+}
 }
 
 export default App;
