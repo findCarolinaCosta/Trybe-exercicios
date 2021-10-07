0378d9e0eff3a4332b07c90eddaf5283bae1965d
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-10-07 15:38:34 -0300

    (11.1) Requisito 2 de fixação da introdução aos eventos

--- 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formulario-com-react/dia-01-componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
+++ 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formulario-com-react/dia-01-componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
@@ -1,5 +1,5 @@
 // Para fixar
-// 1 - Crie uma aplicação React com npx create-react-app fancy-buttons . Altere o componente App.js para que seja um componente de classe e contenha um botão associado a um evento que imprime um texto qualquer via console.log() . Não precisa se preocupar com a sintaxe correta para funções de eventos por enquanto!!
+// 2 - Faça com que sua aplicação exiba três botões lado a lado com textos diferentes. Cada botão clicado deve acionar um evento diferente, cada um escrevendo algo diferente no console do navegador via console.log() .
 
 import React, { Component } from 'react';
 import './App.css';
@@ -8,8 +8,17 @@ import './App.css';
 não é a maneira correta de se criar eventos
 em React! A função se refere ao componente,
 então deve ser parte de sua classe! */
-function handleClick() {
-  console.log('Clicou no botão!')
+
+function handleBtnOne() {
+  console.log('Clicou no botão 1!')
+}
+
+function handleBtnTwo() {
+  console.log('Clicou no botão 2!')
+}
+
+function handleBtnThree() {
+  console.log('Clicou no botão 3!')
 }
 
 class App extends Component {
@@ -17,7 +26,13 @@ class App extends Component {
   JSX você associa uma função a um evento
   passando a própria função entre chaves `{}` */
   render() {
-    return <button onClick={handleClick}>Meu botão</button>
+    return (
+      <div>
+        <button onClick={handleBtnOne}>Meu botão 1</button>
+        <button onClick={handleBtnTwo}>Meu botão 2</button>
+        <button onClick={handleBtnThree}>Meu botão 3</button>
+      </div>
+    );
   }
 }
 
