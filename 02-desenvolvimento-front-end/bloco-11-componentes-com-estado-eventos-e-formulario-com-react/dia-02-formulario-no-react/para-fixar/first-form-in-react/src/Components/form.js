13295f7767da4f40ec986b045ed01f01c8a61adc
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-10-08 18:23:48 -0300

    (11.2) Encapsulando alguns campos num fieldset separados

--- 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formulario-com-react/dia-02-formulario-no-react/para-fixar/first-form-in-react/src/Components/form.js
+++ 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formulario-com-react/dia-02-formulario-no-react/para-fixar/first-form-in-react/src/Components/form.js
@@ -26,36 +26,46 @@ class Form extends Component {
         <h1>Fixando como faz formulário em react com componentes controlados:</h1>
         <h2>Para fixar</h2>
         <p>
-          5 - Busque na documentação de React acerca de formulários (primeiro link da seção de Recursos Adicionais!) como se deve adicionar um input para arquivos . Acrescente um ao seu formulário.
+          6 - Encapsule alguns dos seus campos num fieldset .
         </p>
         <form>
-          <label>
-            <p>Campo de select: </p>
-            <select name='selectFild' value={this.state.selectFild} onChange={this.handleChange} >
-              <option>Select 1</option>
-              <option>Select 2</option>
-              <option>Select 3</option>
-              <option>Select 4</option>
-            </select>
-          </label>
-          <label>
-            <p>Primeiro imput tipo texto: </p>
-            <input name='imputText' type="text" value={this.state.imputText} onChange={this.handleChange} />
-          </label>
-          <label>
-            <p>Text area: </p>
-            <textarea name='textArea' value={this.state.textArea} onChange={this.handleChange} />
-          </label>
-          <div>
+          <fieldset>
+            <legend>Opção de select</legend>
             <label>
-              Segundo imput tipo checkbox:
-              <input type="checkbox" name='imputCheckbox' value={this.state.imputCheckbox} onChange={this.handleChange} />
+              <p>Campo de select: </p>
+              <select name='selectFild' value={this.state.selectFild} onChange={this.handleChange} >
+                <option>Select 1</option>
+                <option>Select 2</option>
+                <option>Select 3</option>
+                <option>Select 4</option>
+              </select>
             </label>
-          </div>
-          <label>
-            Terceiro imput tipo file:
-            <input type="file" name='imputFile' value={this.state.imputFile} onChange={this.handleChange} />
-          </label>
+          </fieldset>
+          <fieldset>
+            <legend>Textos</legend>
+            <label>
+              <p>Primeiro imput tipo texto: </p>
+              <input name='imputText' type="text" value={this.state.imputText} onChange={this.handleChange} />
+            </label>
+            <label>
+              <p>Text area: </p>
+              <textarea name='textArea' value={this.state.textArea} onChange={this.handleChange} />
+            </label>
+          </fieldset>
+          <fieldset>
+            <legend>Outros</legend>
+            <div>
+              <label>
+                Segundo imput tipo checkbox:
+                <input type="checkbox" name='imputCheckbox' value={this.state.imputCheckbox} onChange={this.handleChange} />
+              </label>
+            </div>
+            <label>
+              Terceiro imput tipo file:
+              <input type="file" name='imputFile' value={this.state.imputFile} onChange={this.handleChange} />
+            </label>
+          </fieldset>
+
         </form>
 
         <p>💡 Atenção!Essa nomenclatura, oficial do React, é confusa.Estamos dizendo aqui que o elemento do formulário é um componente controlado.Não estamos falando dos componentes React aqui, mas dos elementos que compõem o formulário!Cuidado para não confundir.</p>
