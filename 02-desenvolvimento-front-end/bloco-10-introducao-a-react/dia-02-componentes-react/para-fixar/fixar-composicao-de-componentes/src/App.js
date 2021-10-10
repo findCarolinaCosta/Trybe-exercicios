f438347b8254881c10f348af655630a0c1d9d9ad
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-10-10 19:56:28 -0300

    (10.2) Refatora app para renderizar os dois exemplos de composição de componentes

--- 02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/para-fixar/fixar-composicao-de-componentes/src/App.js
+++ 02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/para-fixar/fixar-composicao-de-componentes/src/App.js
@@ -1,45 +1,30 @@
 import { Component } from 'react'
 import './App.css';
 import Album from './components/Album';
+import UserProfile from './components/UserProfile';
+import { album01, album02 } from './data-ex1';
+import { joao, amelia } from './data-ex2';
 
 class App extends Component {
   render() {
-    // Declaração do objeto contendo informações do album01
-    const album01 = {
-      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
-      title: 'Mylo Xyloto',
-      releaseDate: {
-        year: '2011',
-        month: '10',
-        day: '24',
-      },
-      others: {
-        recordCompany: 'Capitol, Parlophone',
-        formats: 'CD, digital'
-      }
-    };
-
-    // Declaração do objeto contendo informações do album02
-    const album02 = {
-      image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
-      title: 'Ghost Stories',
-      releaseDate: {
-        year: '2014',
-        month: '05',
-        day: '16',
-      },
-      others: {
-        recordCompany: 'Parlophone',
-        formats: 'CD, digital'
-      }
-    };
-
-    // Retorno do que será renderizado
     return (
       <div>
-        <p>Importante notar que os dois componentes irmãos, são, na realidade, o mesmo componente, porém reutilizados** com base nas props recebidas. Ou seja, apesar de serem o mesmo componente, renderizam informações diferentes, uma vez que recebem props diferentes.</p>
-        <Album album={album01} />
-        <Album album={album02} />
+        <div>
+          <h1>Exemplo 1</h1>
+          <p>Importante notar que os dois componentes irmãos, são, na realidade, o mesmo componente, porém reutilizados** com base nas props recebidas. Ou seja, apesar de serem o mesmo componente, renderizam informações diferentes, uma vez que recebem props diferentes.</p>
+          <Album album={album01} />
+          <Album album={album02} />
+        </div>
+        <hr />
+        <div>
+          <h1>Exemplo 2</h1>
+          <UserProfile user={joao} />
+          <UserProfile user={amelia} />
+        </div>
+        <p>Analisando o código do exemplo 2, temos: <br />
+          1. O componente App é composto por dois componentes UserProfile . Em outras palavras, o componente App é pai dos dois componentes UserProfile . Além disso, user=joao e user=amelia são componentes irmãos , e eles dois são filhos do componente App .<br />
+          2. O componente UserProfile , por sua vez, possui um componente Image . Ou seja, UserProfile tem Image como filho. <br />
+          3. Os dados são repassados de componente pai para componente(s) filho(s). Olhando para o código acima, o componente App , que é pai dos dois componentes UserProfile , passa para cada um de seus filhos um objeto com as informações de um perfil. O mesmo pode ser dito olhando para UserProfile , que passa para seu componente filho Image a origem de uma imagem.</p>
       </div>
     );
   }
