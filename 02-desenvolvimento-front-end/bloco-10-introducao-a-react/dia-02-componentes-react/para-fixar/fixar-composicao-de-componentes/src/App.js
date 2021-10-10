e08c4f43f13d1c18a5c4c3f3beea2ee1e0985200
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-10-10 20:34:27 -0300

    (10.2) etilização básica nos exemplos de Composição de componentes

--- 02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/para-fixar/fixar-composicao-de-componentes/src/App.js
+++ 02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/para-fixar/fixar-composicao-de-componentes/src/App.js
@@ -8,23 +8,23 @@ import { joao, amelia } from './data-ex2';
 class App extends Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className='container'>
+        <div className='div-internal'>
           <h1>Exemplo 1</h1>
           <p>Importante notar que os dois componentes irmãos, são, na realidade, o mesmo componente, porém reutilizados** com base nas props recebidas. Ou seja, apesar de serem o mesmo componente, renderizam informações diferentes, uma vez que recebem props diferentes.</p>
           <Album album={album01} />
           <Album album={album02} />
         </div>
         <hr />
-        <div>
+        <div className='div-internal'>
           <h1>Exemplo 2</h1>
           <UserProfile user={joao} />
           <UserProfile user={amelia} />
+          <p>Analisando o código do exemplo 2, temos: <br />
+            1. O componente App é composto por dois componentes UserProfile . Em outras palavras, o componente App é pai dos dois componentes UserProfile . Além disso, user=joao e user=amelia são componentes irmãos , e eles dois são filhos do componente App .<br />
+            2. O componente UserProfile , por sua vez, possui um componente Image . Ou seja, UserProfile tem Image como filho. <br />
+            3. Os dados são repassados de componente pai para componente(s) filho(s). Olhando para o código acima, o componente App , que é pai dos dois componentes UserProfile , passa para cada um de seus filhos um objeto com as informações de um perfil. O mesmo pode ser dito olhando para UserProfile , que passa para seu componente filho Image a origem de uma imagem.</p>
         </div>
-        <p>Analisando o código do exemplo 2, temos: <br />
-          1. O componente App é composto por dois componentes UserProfile . Em outras palavras, o componente App é pai dos dois componentes UserProfile . Além disso, user=joao e user=amelia são componentes irmãos , e eles dois são filhos do componente App .<br />
-          2. O componente UserProfile , por sua vez, possui um componente Image . Ou seja, UserProfile tem Image como filho. <br />
-          3. Os dados são repassados de componente pai para componente(s) filho(s). Olhando para o código acima, o componente App , que é pai dos dois componentes UserProfile , passa para cada um de seus filhos um objeto com as informações de um perfil. O mesmo pode ser dito olhando para UserProfile , que passa para seu componente filho Image a origem de uma imagem.</p>
       </div>
     );
   }
