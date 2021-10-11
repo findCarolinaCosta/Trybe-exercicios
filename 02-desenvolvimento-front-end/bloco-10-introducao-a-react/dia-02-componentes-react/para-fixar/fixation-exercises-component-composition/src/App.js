3d6c244e7189b2de4652d1b85b9e60649fbafb05
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-10-10 21:23:26 -0300

    (10.2) Rederiza na tela as questões e respostas do exercicio de fixação: composição de componentes

--- 02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/para-fixar/fixation-exercises-component-composition/src/App.js
+++ 02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/para-fixar/fixation-exercises-component-composition/src/App.js
@@ -30,7 +30,15 @@ class App extends React.Component {
         <h1> Orders recently created </h1>
         <Order order={headphone} />
         <Order order={energyDrink} />
-      </div>
+        <p>
+          1. O que o componente App é em relação a Order ?<br />
+          É o pai responsavel por renderizar os componentes filhos.<br />
+        </p>
+        <p>
+          2. Complete o código acima de forma que os pedidos referentes aos produtos headphone e energyDrink sejam filhos de App <br />
+          ** Linha 30 e 31 <span style={{ fontSize: '30px', fontWeight: 'bold' }}>&#10003;</span>
+        </p>
+      </div >
     );
   }
 }
