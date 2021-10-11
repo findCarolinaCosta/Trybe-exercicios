de0cd6a8e30691c9c933bca4a31df91fc865a650
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-10-10 22:52:46 -0300

    (10.2) Adiciona exemplo de props default

--- 02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/para-fixar/fixar-proptypes-checagem-de-tipos/src/components/Greeting.js
+++ 02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/para-fixar/fixar-proptypes-checagem-de-tipos/src/components/Greeting.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'; // 1º passo
 
 class Greeting extends React.Component {
   render() {
-    return (<h1>Hello, {this.props.name} {this.props.lastName}</h1>);
+    const { name, lastName } = this.props;
+    return (<h1>Hello, {name} {lastName}</h1>);
   }
 }
 
@@ -11,5 +12,9 @@ Greeting.propTypes = { //2º passo
   name: PropTypes.string.isRequired,
   lastName: PropTypes.string.isRequired,
 }; //isRequired em casos que props são essenciais para o funcionamento de qualquer componente. Caso contrário fica como validadores opcionais.
+
+Greeting.defaultProps = {
+  name: 'Strange'
+}; // pode-se determinar uma prop default para as props esperadas caso não fores informadas
 
 export default Greeting;
