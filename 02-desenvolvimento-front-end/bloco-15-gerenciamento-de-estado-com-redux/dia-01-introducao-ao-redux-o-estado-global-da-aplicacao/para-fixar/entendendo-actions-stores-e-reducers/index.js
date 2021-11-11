cf6babc7c36f95bcc4b19193509119a3f67b0685
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-11 15:58:25 -0300

    [15.1] para fixar - adiciona action para o caso de login

--- 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-01-introducao-ao-redux-o-estado-global-da-aplicacao/para-fixar/entendendo-actions-stores-e-reducers/index.js
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-01-introducao-ao-redux-o-estado-global-da-aplicacao/para-fixar/entendendo-actions-stores-e-reducers/index.js
@@ -1,5 +1,11 @@
 const Redux = require('redux');
 
+const fazerLogin = (email, password) => ({
+  type: "LOGIN",
+  email,
+  password,
+});
+
 const ESTADO_INICIAL = {
   login: false,
   email: '',
@@ -7,13 +13,23 @@ const ESTADO_INICIAL = {
 };
 
 
-const reducer = (state = ESTADO_INICIAL) => {
-  return state;
+const reducer = (state = ESTADO_INICIAL, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        login: !state.login,
+        email: action.email,
+        password: action.password,
+      }
+    default: // No switch, sempre precisamos ter um caso default!
+      return state;
+  }
 };
 
 const store = Redux.createStore(reducer);
 
-console.log(store.getState()); //{ login: false, email: '', password: '' }
+console.log(store.getState()); //{ login: true, email: 'alguem@email.com', password: '12345678' }
 
 
-// Nosso reducer está montado e possui o nosso estado inicial da aplicação. Vamos verificar o output quando acessamos a store com a função getState() .
+// Mas e se precisarmos alterar o dado que está no estado? A peça que tem esta função é a action ! Uma action é um objeto JavaScript que tem pelo menos uma propriedade type e é responsável por comunicar ao reducer uma mudança a ser feita na store . Em Redux nós utilizamos o actionCreator , que nada mais do que uma função que retorna uma action . Para o nosso exemplo, iremos usar uma actionCreator chamada fazerLogin . Esta função irá enviar uma action ao nosso reducer , com a intenção de alterar para verdadeiro a chave login da nossa store .
