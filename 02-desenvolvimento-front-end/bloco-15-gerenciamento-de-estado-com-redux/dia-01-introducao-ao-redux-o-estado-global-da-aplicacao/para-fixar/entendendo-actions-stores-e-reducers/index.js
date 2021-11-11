247e9494abab91e40d7c1c3750240a239b41e517
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-11 15:41:52 -0300

    [15.1] para fixar - cria função reducer para alterar dados do store

--- 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-01-introducao-ao-redux-o-estado-global-da-aplicacao/para-fixar/entendendo-actions-stores-e-reducers/index.js
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-01-introducao-ao-redux-o-estado-global-da-aplicacao/para-fixar/entendendo-actions-stores-e-reducers/index.js
@@ -1,2 +1,10 @@
 const Redux = require('redux');
-const store = Redux.createStore();
+
+const reducer = (state) => {
+  return state;
+};
+
+const store = Redux.createStore(reducer);
+
+
+//Uma store só funciona se passarmos uma função que será responsável por alterar os dados dela: o reducer . O reducer recebe como primeiro parâmetro um state, sendo que seu retorno substituirá o state da store . Para fins didáticos, iremos montar o reducer no mesmo arquivo, mas a boa prática é fazer em um arquivo separado.
