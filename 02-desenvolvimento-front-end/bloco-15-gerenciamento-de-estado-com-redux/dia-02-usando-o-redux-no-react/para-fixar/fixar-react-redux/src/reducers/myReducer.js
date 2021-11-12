57bd592a606e3b697988f106703294ce48b7aa24
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-12 15:56:08 -0300

    [15.2] Para fixar - adiciona uma action e comenta o que essa action normalmente deve ter

--- 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-02-usando-o-redux-no-react/para-fixar/fixar-react-redux/src/reducers/myReducer.js
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-02-usando-o-redux-no-react/para-fixar/fixar-react-redux/src/reducers/myReducer.js
@@ -1,10 +1,12 @@
+import { NEW_ACTION } from "../actions";
+
 const INITIAL_STATE = {
   state: '',
 };
 
 function myReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case 'NEW_ACTION':
+    case NEW_ACTION:
       return { state: action.state };
     default:
       return state;
