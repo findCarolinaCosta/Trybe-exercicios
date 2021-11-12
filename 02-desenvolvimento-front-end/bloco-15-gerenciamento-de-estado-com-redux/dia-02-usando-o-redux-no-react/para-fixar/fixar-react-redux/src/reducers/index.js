e23da5f763e03b2c6a979a2cb7085bb38351ff41
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-12 15:26:56 -0300

    [15.2] Para fixar - Adiciona comentário da importância de usar o combineReducers

--- 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-02-usando-o-redux-no-react/para-fixar/fixar-react-redux/src/reducers/index.js
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-02-usando-o-redux-no-react/para-fixar/fixar-react-redux/src/reducers/index.js
@@ -3,4 +3,7 @@ import myReducer from './myReducer';
 
 const rootReducer = combineReducers({ myReducer });
 
+//combineReducers: Sem ele, só poderíamos usar um reducer em nossa aplicação.
+// /Dica: Mesmo que tenhamos apenas um reducer é uma boa prática que utilizemos o combineReducers , pois caso nossa aplicação cresça e necessite de mais de um reducer não será necessário alterar sua lógica.
+
 export default rootReducer;
