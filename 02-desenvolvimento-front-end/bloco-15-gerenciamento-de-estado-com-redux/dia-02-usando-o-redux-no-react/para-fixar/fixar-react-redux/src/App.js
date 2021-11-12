8bbd35de97b5631c732fc18e76a684bbe4a9b95a
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-12 15:05:06 -0300

    [15.2] Adiciona arquivos iniciais para fixar react-redux

---
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-02-usando-o-redux-no-react/para-fixar/fixar-react-redux/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Iniciando o exercício para fixar conexão do react com redux</h1>
+    </div>
+  );
+}
+
+export default App;
