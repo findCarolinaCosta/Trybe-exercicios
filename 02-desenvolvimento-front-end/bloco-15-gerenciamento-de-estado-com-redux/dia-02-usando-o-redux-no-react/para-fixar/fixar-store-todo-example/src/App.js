be0f77986489354f1015ce600c5d08f6c1eb5607
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-12 20:56:22 -0300

    [15.2] Para fixar - adiciona arquivos base do exemplo do to do em react-redux

---
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-02-usando-o-redux-no-react/para-fixar/fixar-store-todo-example/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Inicia exercicio para fixar react-redux com um exemplo de todo</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
