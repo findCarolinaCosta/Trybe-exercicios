562fd0323016404306c93730caf55469b242d00d
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-12 22:12:19 -0300

    [15.2] Para fixar exemplo to do - adiciona nova switch case no reducer

--- 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-02-usando-o-redux-no-react/para-fixar/fixar-store-todo-example/src/reducers/index.js
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-02-usando-o-redux-no-react/para-fixar/fixar-store-todo-example/src/reducers/index.js
@@ -4,6 +4,9 @@ function listReducer(state = INITIAL_STATE, action) {
   switch(action.type) {
     case 'ADD_ELEMENT':
       return [...state, action.value];
+      case 'NEW_ACTION':
+        console.log(action.value);
+        return [...state, action.state]
     default:
       return state;
   }
