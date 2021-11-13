152a4201eba08a199301661290c3c2bd29b159ee
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-13 20:21:16 -0300

    [12.1] Adiciona cometário do output obtido de cada ciclo

--- 02-desenvolvimento-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-01-ciclo-de-vida-de-componentes/para-fixar/entendendo-quando-cada-metodo-e-chamado/src/Components/Counter.js
+++ 02-desenvolvimento-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-01-ciclo-de-vida-de-componentes/para-fixar/entendendo-quando-cada-metodo-e-chamado/src/Components/Counter.js
@@ -39,3 +39,20 @@ class Counter extends Component {
 }
 
 export default Counter;
+
+
+//Assim que a tela carrega: Os métodos shouldComponentUpdate e componentDidUpdate não apareceram no console, pois não houve atualização.
+/* 
+outputs em ordem
+construtor
+render
+componentDidMount
+ */
+
+// Quando clica no botão:  o método render é chamado novamente. Isso acontece porque, quando se atualiza uma props ou estado, o React "pede" que o componente seja renderizado no DOM. Como apresentamos acima, caso seja válido, podemos impedir essa renderização retornando false com o método shouldComponentUpdate .
+/* 
+outputs em ordem ao clicar no botão
+shouldComponentUpdate {counter: 0} estado atual {counter: 1} próximo estado
+render
+componentDidUpdate {counter: 1} estado atual {counter: 0} estado anterior
+ */
