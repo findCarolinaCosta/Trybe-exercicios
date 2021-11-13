5eab1f234a87b899e078d8d9690a4f55cc29865c
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-13 01:58:11 -0300

    [15.2] Adiciona arquivos do ex 2 para fixar funcoes reducer e rootReducer

---
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-02-usando-o-redux-no-react/para-fixar/exercises-redux-step-by-step/exercicio-2/missing_reducers/src/reducers/Europe.js
@@ -0,0 +1,18 @@
+import { EUROPE_ENERGY } from '../actions';
+
+const INITIAL_STATE = {
+  percentage: 0,
+};
+
+function europeContinent(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case EUROPE_ENERGY:
+    return {
+      percentage: action.percentage,
+    };
+  default:
+    return state;
+  }
+}
+
+export default europeContinent;
