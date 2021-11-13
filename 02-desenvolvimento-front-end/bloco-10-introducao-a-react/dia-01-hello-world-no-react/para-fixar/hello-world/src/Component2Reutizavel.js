95b5235dca1f7d06bbd95d27228aabe999acd8c3
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-13 17:01:37 -0300

    [10.1]  Adiciona estilização de disposição dos elementos

--- 02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/para-fixar/hello-world/src/Component2Reutizavel.js
+++ 02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/para-fixar/hello-world/src/Component2Reutizavel.js
@@ -6,7 +6,7 @@ export default class ReactClass extends React.Component {
       <div className='container-div'> {/* css herdado do app */}
         <h1>My first React Class Component!</h1>
           <p>Exemplo de componente de classe que rederizaria um input em diferentes locais que fosse necessário:</p>
-          <input /><button>Submit</button>
+          <div><input /><button>Submit</button></div>
       </div>
     )
   }
