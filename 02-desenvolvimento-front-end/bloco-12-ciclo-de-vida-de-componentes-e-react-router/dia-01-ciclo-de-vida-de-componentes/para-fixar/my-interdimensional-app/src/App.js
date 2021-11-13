ad1c20901519d3cdf12c57ace5960262e98fcca2
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-13 20:57:48 -0300

    [12.1] Fixar - Adicona lógica para rederizar array do estado e comentario do porque não funciona ainda

--- 02-desenvolvimento-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-01-ciclo-de-vida-de-componentes/para-fixar/my-interdimensional-app/src/App.js
+++ 02-desenvolvimento-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-01-ciclo-de-vida-de-componentes/para-fixar/my-interdimensional-app/src/App.js
@@ -18,14 +18,26 @@ class App extends Component {
   }
 
   render() {
+    const { characters } = this.state;
     return (
       <div className="App">
         <h1>
           Ricky and Morty Characters:
         </h1>
+        <div className="body">
+          {characters.map(({ name, image }) => {
+            return (
+              <div className="container" key={name}> {/* as keys são usadas pelo react para identificar quais itens sofreram alterações, para que se for preciso rederizar novamente somente pela key modificada e não toda uma lista, etc...*/}
+                <h3>{name}</h3>
+                <img src={image} alt={name}/>
+              </div>
+            )
+          })}
+        </div>
       </div>
     );
   }
 }
+//ainda não funcionando: já que characters é um array vazio ainda, porque o problema do código é lido antes da API retornar.
 
 export default App;
