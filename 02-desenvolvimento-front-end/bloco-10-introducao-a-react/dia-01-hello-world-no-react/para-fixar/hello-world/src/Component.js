a118a46430aab0eb01cd457edec0ea958b59694c
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-13 16:46:21 -0300

    [10.1] Fixando como importar css no react

--- 02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/para-fixar/hello-world/src/Component.js
+++ 02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/para-fixar/hello-world/src/Component.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import './Component.css'
+import './Component.css' //recomendado um css para cada componente, apesar que se aplicado no app, por exemplo, a estilização é herdada.
 
 class AboutMe extends Component {
   render() {
     return (
       <div>
         <div className='name'><h1>Carolina Pereira da Costa</h1></div>
-        <div className='description'><p>Futura desenvolvedora web</p></div>
-        <div className='description'><p>Meu primeiro Hello world com react</p></div >
-        <div className='description'><h2>It is {new Date().toLocaleTimeString()}.</h2></div>
+        <div className='container-div'><p>Futura desenvolvedora web</p></div>
+        <div className='container-div'><p>Meu primeiro Hello world com react</p></div >
+        <div className='container-div'><h2>It is {new Date().toLocaleTimeString()}.</h2></div>
       </div >
     );
   };
