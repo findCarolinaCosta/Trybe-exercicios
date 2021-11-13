8894aa3bcc6995cdd4e0bd799b892b1205a59003
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-12 22:14:03 -0300

    [15.2] Para fixar exemplo to do -  adiciona segundo paremetro para extenção redux dev tools

--- 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-02-usando-o-redux-no-react/para-fixar/fixar-store-todo-example/src/store/index.js
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-02-usando-o-redux-no-react/para-fixar/fixar-store-todo-example/src/store/index.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import listReducer from '../reducers';
 
 const rootReducer = combineReducers({ listReducer });
 
-const store = createStore(rootReducer);
+const extension = window.devToolsExtension() || ((f) => f);
+
+const store = createStore(rootReducer, compose(extension));
 
 export default store;
