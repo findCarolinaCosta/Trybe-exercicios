047c0ad4796167434407ef0c0face39243312340
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-16 21:41:36 -0300

    [15.3] Adiciona Header na Home page

--- 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-03-usando-o-redux-no-react-pratica/exercicios/customer-base/src/Pages/Home/index.js
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-03-usando-o-redux-no-react-pratica/exercicios/customer-base/src/Pages/Home/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from '../../Components/Header/';
 
 class Home extends React.Component {
   render() {
     return (
       <>
+        <Header />
         <h1>Sistema de cadatro de clientes</h1>
       </>
     )
