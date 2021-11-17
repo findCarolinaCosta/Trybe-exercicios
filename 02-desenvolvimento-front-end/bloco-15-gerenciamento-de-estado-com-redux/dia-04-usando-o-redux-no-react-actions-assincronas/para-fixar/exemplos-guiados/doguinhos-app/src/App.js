8789c31b65e5204796613d2465916e6244d00db1
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-17 15:52:00 -0300

    [15.4] Adiciona código dado como exemplo no app

--- 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-04-usando-o-redux-no-react-actions-assincronas/para-fixar/exemplos-guiados/doguinhos-app/src/App.js
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-04-usando-o-redux-no-react-actions-assincronas/para-fixar/exemplos-guiados/doguinhos-app/src/App.js
@@ -1,9 +1,32 @@
-function App() {
-  return (
-    <div className="App">
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchDog } from './store';
 
-    </div>
+function App({ isFetching, src, fetchDog }) {
+  return (
+    isFetching ? <p>Loading</p>
+      : (
+        <div style={{ width: 500 }}>
+          <button
+            style={{ display: 'block' }}
+            onClick={fetchDog}
+            type="button"
+          >
+            Novo Doguinho
+          </button>
+          <img style={{ width: '100%' }} src={src} alt="dog" />
+        </div>
+      )
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  src: state.imagePath,
+  isFetching: state.isFetching
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchDog: () => dispatch(fetchDog())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
