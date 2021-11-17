10be915542195626c53195edd6b0e3b81b3b63d6
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-16 21:40:20 -0300

    [15.3] Adiciona home como rota raiz no app

--- 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-03-usando-o-redux-no-react-pratica/exercicios/customer-base/src/App.js
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-03-usando-o-redux-no-react-pratica/exercicios/customer-base/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./Pages/Home/";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
