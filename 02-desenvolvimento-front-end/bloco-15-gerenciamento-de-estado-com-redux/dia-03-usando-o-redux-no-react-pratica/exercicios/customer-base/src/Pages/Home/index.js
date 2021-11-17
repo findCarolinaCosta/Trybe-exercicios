e7c1738a1f0659b801dacc92b6ef2b27714aa3a5
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-16 21:38:36 -0300

    [15.3] Adiciona página home

---
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-03-usando-o-redux-no-react-pratica/exercicios/customer-base/src/Pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Sistema de cadatro de clientes</h1>
+      </>
+    )
+  }
+}
+
+export default Home;
