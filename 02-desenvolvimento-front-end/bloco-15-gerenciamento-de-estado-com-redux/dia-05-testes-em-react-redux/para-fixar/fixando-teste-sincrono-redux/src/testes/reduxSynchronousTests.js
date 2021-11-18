4f4de1d8e85a805887c546f7647e7ca43f85265c
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-18 16:09:50 -0300

    [15.5] Adiciona função renderWithRedux com adaptação para a combinação de reducers

--- 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-05-testes-em-react-redux/para-fixar/fixando-teste-sincrono-redux/src/testes/reduxSynchronousTests.js
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-05-testes-em-react-redux/para-fixar/fixando-teste-sincrono-redux/src/testes/reduxSynchronousTests.js
@@ -1,8 +1,18 @@
-//Para utilizar a função renderWithRedux em nossos testes, é necessário também fazer alguns imports :
-
 import React from 'react'
 import { Provider } from 'react-redux'
 import { render, cleanup } from '@testing-library/react';
 import App from './App';
+import { createStore, combineReducers } from 'redux';
+import clickReducer from './reducers';
 
-//Esses imports servem para termos acesso às ferramentas necessárias pra mockar o store 
+// Um detalhe muito importante: como é utilizado um combineReducers na resolução do exercício, quando formos implementar os testes, temos que criar o store mockado com a mesma estrutura do padrão. Portanto, vamos importar o combineReducers e adaptar a função renderWithRedux para utilizá-lo.
+
+const renderWithRedux = (
+  component,
+  { initialState, store = createStore(combineReducers({ clickReducer }), initialState) } = {}
+) => {
+  return {
+    ...render(<Provider store={store}>{component}</Provider>),
+    store,
+  }
+}
