27f7994ac7b825d371627a2d3569d0b5050f4856
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-18 16:17:55 -0300

    [15.5] Adaptações  concluídas Agora os testes ...!

--- 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-05-testes-em-react-redux/para-fixar/fixando-teste-sincrono-redux/src/testes/reduxSynchronousTests.js
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-05-testes-em-react-redux/para-fixar/fixando-teste-sincrono-redux/src/testes/reduxSynchronousTests.js
@@ -5,8 +5,6 @@ import App from './App';
 import { createStore, combineReducers } from 'redux';
 import clickReducer from './reducers';
 
-// Um detalhe muito importante: como é utilizado um combineReducers na resolução do exercício, quando formos implementar os testes, temos que criar o store mockado com a mesma estrutura do padrão. Portanto, vamos importar o combineReducers e adaptar a função renderWithRedux para utilizá-lo.
-
 const renderWithRedux = (
   component,
   { initialState, store = createStore(combineReducers({ clickReducer }), initialState) } = {}
