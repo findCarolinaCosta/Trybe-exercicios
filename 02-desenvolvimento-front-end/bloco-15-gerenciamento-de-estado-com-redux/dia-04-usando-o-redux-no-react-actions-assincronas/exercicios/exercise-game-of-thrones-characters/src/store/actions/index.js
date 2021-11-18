def47b98687d4e39a7e6ec34b3933f4fbf9f19c4
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-18 05:02:00 -0300

    [15.4] Adiciona action assíncrona Que dispara action de resolvido ou rejeitado a depender da resposta da api

--- 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-04-usando-o-redux-no-react-actions-assincronas/exercicios/exercise-game-of-thrones-characters/src/store/actions/index.js
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-04-usando-o-redux-no-react-actions-assincronas/exercicios/exercise-game-of-thrones-characters/src/store/actions/index.js
@@ -1,3 +1,5 @@
+import charAPI from "../../services/charAPI";
+
 export const REQUEST_CHARACTER_API = 'REQUEST_CHARACTER';
 export const REQUEST_CHARACTER_API_RESOLVE = 'REQUEST_CHARACTER_API_RESOLVE';
 export const REQUEST_CHARACTER_API_REJECT = 'REQUEST_CHARACTER_API_REJECT';
@@ -7,3 +9,17 @@ export const requestCharacterApi = () => ({ type: REQUEST_CHARACTER_API });
 export const getResolveInRequestApi = (payload) => ({ type: REQUEST_CHARACTER_API_RESOLVE, payload });
 
 export const getRejectInRequestApi = (error) => ({ type: REQUEST_CHARACTER_API_REJECT, error });
+
+export function requestCharacterThunk(name) {
+  return async (dispatch) => {
+    dispatch(requestCharacterApi())
+    try {
+      const response = await charAPI(name);
+      const character = response[0];
+      dispatch(getResolveInRequestApi(character));
+    } catch (error) {
+      console.log(error);
+      dispatch(getRejectInRequestApi('Não encontrado!'));
+    }
+  }
+}
