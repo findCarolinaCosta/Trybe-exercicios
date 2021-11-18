7a064430557a5ac0c164e30a4d38030f96209789
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-18 16:16:08 -0300

    [15.5] Move Provider para index do diretório do src Para que nos testes  pssa se criar store mockado

--- 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-05-testes-em-react-redux/para-fixar/fixando-teste-sincrono-redux/src/index.js
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-05-testes-em-react-redux/para-fixar/fixando-teste-sincrono-redux/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
+
+// Store da aplicação transferido para index. Para que nos testes, seja prover um novo store para podermos controlá-lo envolvendo ao App.
+// Logo quando renderizar o componente <App /> nos testes nós não renderizamos o store da aplicação, que está fora desse componente. Assim ficamos livres para criar um novo store que podemos controlar no ambiente de testes!
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
