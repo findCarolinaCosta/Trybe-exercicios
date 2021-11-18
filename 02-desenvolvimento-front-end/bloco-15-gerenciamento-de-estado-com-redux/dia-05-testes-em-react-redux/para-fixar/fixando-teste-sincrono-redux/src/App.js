3f43b3b41d742832f6e3704224f6b6398c439104
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-18 16:03:20 -0300

    [15.5] Adiciona arquivos de exemplo contador Para que com eles seja possível fixar os teste sincronos

--- 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-05-testes-em-react-redux/para-fixar/fixando-teste-sincrono-redux/src/App.js
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-05-testes-em-react-redux/para-fixar/fixando-teste-sincrono-redux/src/App.js
@@ -1,9 +1,19 @@
-function App() {
-  return (
-    <div className="App">
-      Adiciona arquivos inciais do react
-    </div>
-  );
+// src/App.js
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+import ButtonClicks from './ButtonClicks';
+import NumberClicks from './NumberClicks';
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Provider store={store}>
+          <ButtonClicks />
+          <NumberClicks />
+        </Provider>
+      </div>
+    );
+  }
 }
-
 export default App;
