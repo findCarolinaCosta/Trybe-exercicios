dc0fb0ea615e24bde72b95052f51b671a97f7f6e
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-19 01:41:41 -0300

    [15.5] Adiciona mock do store

--- 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-05-testes-em-react-redux/exercicios/exercise-react-with-redux-intro-copy/src/tests/helpers/renderWithRedux.js
+++ 02-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-05-testes-em-react-redux/exercicios/exercise-react-with-redux-intro-copy/src/tests/helpers/renderWithRedux.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
 import { render } from '@testing-library/react';
+import CarsReducer from '../../reducers/CarsReducer';
+import TrafficSignalReducer from '../../reducers/TrafficSignalReducer';
+import '@testing-library/jest-dom';
+
+const createMockStore = (initialState) => (
+  createStore(combineReducers(
+    {
+    CarsReducer,
+    TrafficSignalReducer,
+  }
+  ), 
+    initialState
+  )
+);
 
 const renderWithRedux = (
   component, { initialState, store = createMockStore(initialState) } = {},
