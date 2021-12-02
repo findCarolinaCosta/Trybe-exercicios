5fb77902c3b464935cb1aa5e3fac2fd9d25d8d50
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-12-02 16:01:56 -0300

    [17.2] Refatora para componente funcional usando useState

--- 02-desenvolvimento-front-end/bloco-17-context-api-e-react-hooks/dia-02-react-hooks-useState-e-useContext/para-fixar/fixando-react-hooks-usestate-e-usecontext/src/useState/UseState.js
+++ 02-desenvolvimento-front-end/bloco-17-context-api-e-react-hooks/dia-02-react-hooks-useState-e-useContext/para-fixar/fixando-react-hooks-usestate-e-usecontext/src/useState/UseState.js
@@ -1,32 +1,16 @@
+import React, { useState } from "react";
 
-import React, { Component } from "react";
-
-class UseState extends Component {
-  constructor() {
-    super();
-    this.state = {
-      counter: 0,
-    };
-  }
-
-  render() {
-    const { counter } = this.state
-
-    return (
-      <div>
+function UseState() {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
         <h1>Exemplo com useState:</h1>
-        <div>Counter: {counter}</div>
-        <button
-          type="button"
-          onClick={() =>
-            this.setState((prevState) => ({ counter: prevState.counter + 1 }))
-          }
-        >
-          Increase Counter
-        </button>
-      </div>
-    );
-  }
+      <div>Counter: {counter}</div>
+      <button type="button" onClick={() => setCounter(counter + 1)}>
+        Increase Counter
+      </button>
+    </div>
+  );
 }
 
 export default UseState;
