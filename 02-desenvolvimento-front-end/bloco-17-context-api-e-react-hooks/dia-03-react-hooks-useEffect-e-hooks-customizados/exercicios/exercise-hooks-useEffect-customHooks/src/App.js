c467ae0c789f1d166d7197203f2a5a874d15c1df
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-12-04 19:57:40 -0300

    [17.3] Ex1 - Refatora App para componente funcional

--- 02-desenvolvimento-front-end/bloco-17-context-api-e-react-hooks/dia-03-react-hooks-useEffect-e-hooks-customizados/exercicios/exercise-hooks-useEffect-customHooks/src/App.js
+++ 02-desenvolvimento-front-end/bloco-17-context-api-e-react-hooks/dia-03-react-hooks-useEffect-e-hooks-customizados/exercicios/exercise-hooks-useEffect-customHooks/src/App.js
@@ -1,17 +1,23 @@
-import React, { Component } from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import Posts from './components/Posts';
 import Selector from './components/Selector';
 import { Context } from './components/RedditContext';
 
-class App extends Component {
-  componentDidMount() {
-    const { fetchPosts } = this.context;
-    fetchPosts();
-  }
+function App() {
+  const {
+    selectedSubreddit,
+    postsBySubreddit,
+    isFetching,
+    refreshSubreddit,
+    fetchPosts,
+  } = useContext(Context);
 
-  renderLastUpdatedAt() {
-    const { selectedSubreddit, postsBySubreddit } = this.context;
+  useEffect(() => {
+    fetchPosts();
+  }, [fetchPosts]);
+
+  const renderLastUpdatedAt = () =>  {
     const { lastUpdated } = postsBySubreddit[selectedSubreddit];
 
     if (!lastUpdated) return null;
@@ -23,9 +29,7 @@ class App extends Component {
     );
   }
 
-  renderRefreshButton() {
-    const { isFetching, refreshSubreddit } = this.context;
-
+  const renderRefreshButton = () => {
     if (isFetching) return null;
 
     return (
@@ -39,8 +43,6 @@ class App extends Component {
     );
   }
 
-  render() {
-    const { selectedSubreddit, postsBySubreddit, isFetching } = this.context;
     const { items: posts = [] } = postsBySubreddit[selectedSubreddit];
     const isEmpty = posts.length === 0;
 
@@ -48,15 +50,14 @@ class App extends Component {
       <div>
         <Selector />
         <div>
-          {this.renderLastUpdatedAt()}
-          {this.renderRefreshButton()}
+          {renderLastUpdatedAt()}
+          {renderRefreshButton()}
         </div>
         {isFetching && <h2>Loading...</h2>}
         {!isFetching && isEmpty && <h2>Empty.</h2>}
         {!isFetching && !isEmpty && <Posts />}
       </div>
     );
-  }
 }
 
 App.contextType = Context;
