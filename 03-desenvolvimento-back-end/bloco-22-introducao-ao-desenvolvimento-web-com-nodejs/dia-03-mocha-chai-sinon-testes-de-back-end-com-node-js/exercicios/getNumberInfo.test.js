5298bc4d2115e151b03d726cb5ba3ce7fbb187a3
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-02-10 19:24:44 -0300

    [22.3] Add getNumberInfo function test file

---
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-03-mocha-chai-sinon-testes-de-back-end-com-node-js/exercicios/getNumberInfo.test.js
@@ -0,0 +1,41 @@
+const { expect } = require("chai");
+
+const getNumberInfo = require("./getNumberInfo");
+
+describe("Testa função getNumberInfo", () => {
+  describe("Testa retorno positivo esperado", () => {
+    it("resposta é uma string", () => {
+      const answer = getNumberInfo(0);
+      expect(answer).to.be.a("string");
+    });
+
+    it("numero > 0", () => {
+      const answer = getNumberInfo(10);
+      expect(answer).to.be.equals("positivo");
+    });
+  });
+
+  describe("Testa retorno negativo esperado", () => {
+    it("resposta é uma string", () => {
+      const answer = getNumberInfo(0);
+      expect(answer).to.be.a("string");
+    });
+
+    it("numero < 0", () => {
+      const answer = getNumberInfo(-2);
+      expect(answer).to.be.equals("negativo");
+    });
+  });
+
+  describe("Testa retorno neutro esperado", () => {
+    it("resposta é uma string", () => {
+      const answer = getNumberInfo(0);
+      expect(answer).to.be.a("string");
+    });
+
+    it("numero = 0", () => {
+      const answer = getNumberInfo(0);
+      expect(answer).to.be.equals("neutro");
+    });
+  });
+});
