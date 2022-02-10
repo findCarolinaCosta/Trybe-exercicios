6c11d482726edc1bcb705199b8542facd2f04ba3
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-02-10 15:41:03 -0300

    [22.3] Add only comments, no practical example, about end-to-end tests

---
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-03-mocha-chai-sinon-testes-de-back-end-com-node-js/para-fixar/testeEndToEnd.js
@@ -0,0 +1,4 @@
+// Esses testes testam o fluxo de integração completo de uma aplicação
+// Exemplo poderíamos pensar uma API que utiliza nossa calculadora (assim como diversas outras funções mais complexas) na hora de realizar uma operação de venda de produtos. Esse teste é o mais completo pois pressupõe que todos os demais testes estão ou serão desenvolvidos;
+
+//Na prática seria como avaliador testando de ponta-a-ponta pressupondo que toda cadeia de recursos deva estar funcionando
