d2a4ba986d43286299149abd976924849b3dc519
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-02-10 20:18:40 -0300

    [22.3] Adds adaptation of writeFile tests with stub  - Isolate the test.  -  Simulates the interaction, expected behavior.  - Not needing a real file for creating and writing a file.

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-03-mocha-chai-sinon-testes-de-back-end-com-node-js/exercicios/writeFile.test.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-03-mocha-chai-sinon-testes-de-back-end-com-node-js/exercicios/writeFile.test.js
@@ -1,8 +1,18 @@
 const { expect } = require("chai");
+const fs = require("fs");
+const sinon = require("sinon");
 
 const writeFile = require("./writeFile");
 
 describe("Testa função writeFile", () => {
+  before(() => {
+    sinon.stub(fs, "writeFileSync");
+  });
+
+  after(() => {
+    fs.writeFileSync.restore();
+  });
+
   describe("Testa retorno esperado", () => {
     it("resposta é uma string", () => {
       const answer = writeFile("arquivo.txt", "Conteúdo do arquivo de texto");
