76d2215eacb9a269697883b590b3362225efc90c
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-02-10 23:32:10 -0300

    [22.2] Add function that reads simpsons.json  file

---
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-node-js-fluxo-assincrono/exercicios/readSimpsonsFile .js
@@ -0,0 +1,16 @@
+const fs = require("fs").promises;
+
+function readSimpsonsFile() {
+  fs.readFile("./simpsons.json", "utf-8")
+    .then((fileContent) => {
+      return JSON.parse(fileContent);
+    })
+    .then((simpsons) => {
+      return simpsons.map(({ id, name }) => `${id} - ${name}`);
+    })
+    .then((strings) => {
+      strings.forEach((string) => console.log(string));
+    });
+}
+
+readSimpsonsFile();
