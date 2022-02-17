4c040cbb22cabf1a73662ddebbbdcd3ddcd374d1
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-02-17 18:33:24 -0300

    [23.1] Add endPoint GET /books with getAll middleware

--- 03-desenvolvimento-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restfull/dia-01-arquitetura-de-software-camada-model/para-fixar/model-example/index.js
+++ 03-desenvolvimento-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restfull/dia-01-arquitetura-de-software-camada-model/para-fixar/model-example/index.js
@@ -5,11 +5,18 @@ const app = express();
 const Author = require("./models/Author");
 const { writeFile } = require("fs/promises");
 
+const Book = require("./models/Book");
+
 app.get("/authors", async (_req, res) => {
   const authors = await Author.getAll();
 
   await writeFile("./authors.json", JSON.stringify(authors));
   return res.status(200).json(authors);
+});
+
+app.get("/books", async (req, res) => {
+  const books = await Book.getAll();
+  return res.status(200).json(books);
 });
 
 const PORT = process.env.PORT || 3000;
