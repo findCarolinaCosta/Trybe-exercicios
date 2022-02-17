00ad056d902e3e3b1f2495a8d3081cd3582193b3
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-02-17 18:50:45 -0300

    [23.1] Add condition so that if query with author id is passed getByAuthorId, if not getAll

--- 03-desenvolvimento-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restfull/dia-01-arquitetura-de-software-camada-model/para-fixar/model-example/index.js
+++ 03-desenvolvimento-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restfull/dia-01-arquitetura-de-software-camada-model/para-fixar/model-example/index.js
@@ -15,8 +15,13 @@ app.get("/authors", async (_req, res) => {
 });
 
 app.get("/books", async (req, res) => {
-  const books = await Book.getAll();
-  return res.status(200).json(books);
+  const { author_id } = req.query;
+
+  const books = author_id
+    ? await Book.getByAuthorId(author_id)
+    : await Book.getAll();
+
+  res.status(200).json(books);
 });
 
 const PORT = process.env.PORT || 3000;
