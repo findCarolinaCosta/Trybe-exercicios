cd364cda1c24a96233347929dbb08dc9137e392b
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-02-17 19:24:34 -0300

    [23.1] Add POST /authors endpoint for inserting new authors in db

--- 03-desenvolvimento-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restfull/dia-01-arquitetura-de-software-camada-model/para-fixar/model-example/index.js
+++ 03-desenvolvimento-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restfull/dia-01-arquitetura-de-software-camada-model/para-fixar/model-example/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
-
-const app = express();
+const bodyParser = require("body-parser");
 
 const Author = require("./models/Author");
 const { writeFile } = require("fs/promises");
+
+const app = express();
+
+app.use(bodyParser.json());
 
 const Book = require("./models/Book");
 
@@ -40,6 +43,18 @@ app.get("/books/:id", async (req, res) => {
   if (!book) return res.status(404).json({ message: "Not found" });
 
   res.status(200).json(book);
+});
+
+app.post("/authors", async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if (!Author.isValid(first_name, middle_name, last_name)) {
+    return res.status(400).json({ message: "Dados inválidos" });
+  }
+
+  await Author.create(first_name, middle_name, last_name);
+
+  res.status(201).json({ message: "Autor criado com sucesso! " });
 });
 
 const PORT = process.env.PORT || 3000;
