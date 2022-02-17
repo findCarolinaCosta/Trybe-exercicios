6c9374e8d7c432340c25743c3401a67d99842bf3
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-02-17 19:09:52 -0300

    [23.1] Add function that returns book by id

--- 03-desenvolvimento-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restfull/dia-01-arquitetura-de-software-camada-model/para-fixar/model-example/models/Book.js
+++ 03-desenvolvimento-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restfull/dia-01-arquitetura-de-software-camada-model/para-fixar/model-example/models/Book.js
@@ -22,7 +22,21 @@ const getByAuthorId = async (authorId) => {
   }));
 };
 
+const getById = async (id) => {
+  const query = "SELECT * FROM model_example.books WHERE id=?;";
+  const [books] = await connection.execute(query, [id]);
+
+  if (books.length === 0) return;
+
+  return books.map(({ id, title, author_id }) => ({
+    id,
+    title,
+    authorId: author_id,
+  }))[0];
+};
+
 module.exports = {
   getAll,
   getByAuthorId,
+  getById,
 };
