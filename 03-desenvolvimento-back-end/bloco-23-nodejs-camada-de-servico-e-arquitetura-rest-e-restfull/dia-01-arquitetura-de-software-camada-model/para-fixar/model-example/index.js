6afca4f2e458b33e7d0974b2260260011b67f217
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-02-17 15:48:03 -0300

    [23.1] Add GET /authors endpoint saving all registered authors in the author.json file and return data in request too

--- 03-desenvolvimento-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restfull/dia-01-arquitetura-de-software-camada-model/para-fixar/model-example/index.js
+++ 03-desenvolvimento-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restfull/dia-01-arquitetura-de-software-camada-model/para-fixar/model-example/index.js
@@ -2,6 +2,16 @@ const express = require("express");
 
 const app = express();
 
+const Author = require("./models/Author");
+const { writeFile } = require("fs/promises");
+
+app.get("/authors", async (_req, res) => {
+  const authors = await Author.getAll();
+
+  await writeFile("./authors.json", JSON.stringify(authors));
+  return res.status(200).json(authors);
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
