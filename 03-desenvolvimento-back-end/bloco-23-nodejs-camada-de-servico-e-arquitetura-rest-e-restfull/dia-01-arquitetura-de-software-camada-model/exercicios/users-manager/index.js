0a858e3abbcfdf64822f52af81dcca7830d0abff
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-02-18 00:51:33 -0300

    [23.1] Add POST /user endpoint to receive request and run createUser middleware

--- 03-desenvolvimento-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restfull/dia-01-arquitetura-de-software-camada-model/exercicios/users-manager/index.js
+++ 03-desenvolvimento-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restfull/dia-01-arquitetura-de-software-camada-model/exercicios/users-manager/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const middlewares = require("./middlewares");
 
 app.use(bodyParser.json());
+
+app.post("/user", middlewares.createUser);
 
 const PORT = process.env.PORT || 3000;
 
