3783c0b1b5ce912c2d51cbd53e1ba08ee546cf82
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-02-22 21:52:49 -0300

    [23.4] Add GET /movies/:id endpoint to find movies by id

--- 03-desenvolvimento-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restfull/dia-04-arquitetura-de-software-testando-as-camadas/exercicios/movies-api/index.js
+++ 03-desenvolvimento-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restfull/dia-04-arquitetura-de-software-testando-as-camadas/exercicios/movies-api/index.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post("/movies", MovieController.create);
+app.get("/movies/:id", MovieController.findById);
 
 const PORT = process.env.PORT || 3000;
 
