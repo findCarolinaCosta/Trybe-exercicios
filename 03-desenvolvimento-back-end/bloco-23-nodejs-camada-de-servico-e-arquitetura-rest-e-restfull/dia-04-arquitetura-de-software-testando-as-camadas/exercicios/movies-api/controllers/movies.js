241ecab077c836e8148d2e9ba01bcc973a052222
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-02-22 21:46:41 -0300

    Adds a controller layer function that receives the id by the endpoint parameter returns the movie by id - if there is no error during the execution of the other layers - Function that handles request and response from find by id

--- 03-desenvolvimento-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restfull/dia-04-arquitetura-de-software-testando-as-camadas/exercicios/movies-api/controllers/movies.js
+++ 03-desenvolvimento-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restfull/dia-04-arquitetura-de-software-testando-as-camadas/exercicios/movies-api/controllers/movies.js
@@ -21,6 +21,23 @@ const create = async (req, res) => {
   return res.status(201).send("Filme criado com sucesso!");
 };
 
+const findById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movie = await MoviesService.findById(id);
+
+    if (!movie) {
+      return res.status(404).send("Not Found");
+    }
+
+    return res.status(200).json(movie);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: err.message });
+  }
+};
+
 module.exports = {
   create,
+  findById,
 };
