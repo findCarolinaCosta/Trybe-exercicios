f33cb44d31e4b0f06af6547e20ae7a98f6d21be3
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-03-07 16:28:40 -0300

    [24.1] Add seeders code to insert data into Users table with object array structure

--- 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/para-fixar/app-with-sequelize/seeders/20220307190818-users.js
+++ 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/para-fixar/app-with-sequelize/seeders/20220307190818-users.js
@@ -1,24 +1,23 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+  up: async (queryInterface, Sequelize) => queryInterface.bulkInsert('Users',
+  [
+    {
+      fullName: 'Carolina',
+      email: 'carol@test.com',
+      // usamos a função CURRENT_TIMESTAMP do SQL para salvar a data e hora atual nos campos `createdAt` e `updatedAt`
+      createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    {
+      fullName: 'Bruno',
+      email: 'bruno@test.com',
+      createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+  ], {}),
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  // função bulkInsert insere múltiplos dados na tabela
+  down: async (queryInterface) => queryInterface.bulkDelete('Users', null, {}),
 };
