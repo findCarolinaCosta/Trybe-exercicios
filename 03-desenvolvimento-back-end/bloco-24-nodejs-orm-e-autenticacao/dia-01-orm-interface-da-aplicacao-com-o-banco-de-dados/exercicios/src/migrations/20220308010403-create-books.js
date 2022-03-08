8348fe08b83258c770d7500f7934675e9e98ad63
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-03-07 22:37:44 -0300

    [24.1] Add migration create books

---
+++ 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios/src/migrations/20220308010403-create-books.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+
+      title: {
+        type: Sequelize.STRING,
+      },
+
+      author: {
+        type: Sequelize.STRING
+      },
+
+      pageQuantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at'
+      },
+
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated_at'
+      }
+    });
+
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Books');
+  }
+};
