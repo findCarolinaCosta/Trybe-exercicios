06abce9c8203a4fab1b7451fc74daa782574458f
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-03-08 23:23:22 -0300

    [24.2] Add employee integration tests

--- 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations/para-fixar/associations/index.js
+++ 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations/para-fixar/associations/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 const { Address, Employee } = require('./src/models');
 const { Book, User } = require('./src/models');
