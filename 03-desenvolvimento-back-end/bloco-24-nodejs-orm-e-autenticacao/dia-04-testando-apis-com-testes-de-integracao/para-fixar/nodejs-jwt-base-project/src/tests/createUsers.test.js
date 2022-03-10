91b8560e40da42115090e7b6b6d5ee35e0284020
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-03-10 16:26:39 -0300

    [24.4] Adds stub of the "mocked" user's findAll function

--- 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-04-testando-apis-com-testes-de-integracao/para-fixar/nodejs-jwt-base-project/src/tests/createUsers.test.js
+++ 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-04-testando-apis-com-testes-de-integracao/para-fixar/nodejs-jwt-base-project/src/tests/createUsers.test.js
@@ -1,32 +1,44 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const sinon = require("sinon");
 
-const server = require('../api/app');
+const server = require("../api/app");
+
+// Importação do modelo original, contido em `models`
+const { User } = require("../models");
+// Importação do mock utilizado nesse contexto
+const { User: userMock } = require("./mock/models");
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Rota /api/users', () => {
-    describe('Consulta a lista de pessoas usuárias', () => {
-        let response;
+describe("Rota /api/users", () => {
+  before(() => {
+    sinon.stub(User, "findAll").callsFake(userMock.findAll);
+  });
 
-        before(async () => {
-          response = await chai
-              .request(server)
-              .get('/api/users');
-      });
+  after(() => {
+    User.findAll.restore();
+  });
 
-        it(
-            'A requisição GET para a rota traz uma lista inicial ' +
-            'contendo dois registros de pessoas usuárias',
-            () => {
-              expect(response.body).to.have.length(2);
-            }
-        );
+  describe("Consulta a lista de pessoas usuárias", () => {
+    let response;
 
-        it('Essa requisição deve retornar código de status 200', () => {
-            expect(response).to.have.status(200);
-        });
+    before(async () => {
+      response = await chai.request(server).get("/api/users");
     });
+
+    it(
+      "A requisição GET para a rota traz uma lista inicial " +
+        "contendo dois registros de pessoas usuárias",
+      () => {
+        expect(response.body).to.have.length(2);
+      }
+    );
+
+    it("Essa requisição deve retornar código de status 200", () => {
+      expect(response).to.have.status(200);
+    });
+  });
 });
