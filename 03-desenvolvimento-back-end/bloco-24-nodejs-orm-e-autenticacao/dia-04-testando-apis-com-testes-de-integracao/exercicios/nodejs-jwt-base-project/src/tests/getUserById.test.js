1be427d7fac549124b753de71bb4e68d7c6298e6
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-03-11 00:10:18 -0300

    [24.4] Add test for user with valid token to be able to see only their own data

--- 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-04-testando-apis-com-testes-de-integracao/exercicios/nodejs-jwt-base-project/src/tests/getUserById.test.js
+++ 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-04-testando-apis-com-testes-de-integracao/exercicios/nodejs-jwt-base-project/src/tests/getUserById.test.js
@@ -6,57 +6,68 @@ const server = require("../api/app");
 
 const { User } = require("../models");
 const { User: userMock } = require("./mock/models");
-const userDBFake = require('./mock/models/Users.json');
+const userDBFake = require("./mock/models/Users.json");
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
 describe("Rota GET /api/users/:id", () => {
-  describe('Sucesso em fazer requisição com token', () => {
+  describe("Sucesso em fazer requisição com token", () => {
     let response;
-  before(() => {
-    sinon.stub(User, "findOne").callsFake(userMock.findOne);
-  });
-
-  after(() => {
-    User.findOne.restore();
-  });
-
-  it("Efetua login corretamente e retorna token", async () => {
-    response = await chai
-    .request(server)
-    .post("/api/login")
-    .send({
-      username: userDBFake[0].username,
-      password: userDBFake[0].password
+    before(() => {
+      sinon.stub(User, "findOne").callsFake(userMock.findOne);
     });
 
-  expect(response).to.have.status(200);
-  expect(response.body).to.have.property("token");
-  });
+    after(() => {
+      User.findOne.restore();
+    });
 
-  it("requisição com token retorna o esperado", async () => {
-    const {token} = response.body;
-    const userExpect = await chai
-      .request(server)
-      .get('/api/users/1')
-      .set('authorization', token);
+    it("Efetua login corretamente e retorna token", async () => {
+      response = await chai.request(server).post("/api/login").send({
+        username: userDBFake[0].username,
+        password: userDBFake[0].password,
+      });
 
-    expect(userExpect).to.have.status(200);
-    expect(userExpect.body).to.contain(userDBFake[0])
-  });
-  })
-  describe('Falha ao fazer requisição sem token', () => {
-    it('Esperado status 400 e message "Token não ' + 
-    'encontrado ou informado" ao fazer requisição sem token', async () => {
+      expect(response).to.have.status(200);
+      expect(response.body).to.have.property("token");
+    });
+
+    it("requisição com token retorna o esperado", async () => {
+      const { token } = response.body;
+      const userExpect = await chai
+        .request(server)
+        .get("/api/users/1")
+        .set("authorization", token);
+
+      expect(userExpect).to.have.status(200);
+      expect(userExpect.body).to.contain(userDBFake[0]);
+    });
+
+    it("com token válido usuário poderá ver somente os seus próprios dados", async () => {
+      const { token } = response.body;
+
       const userResponse = await chai
         .request(server)
-        .get("/api/users/1");
-  
-      expect(userResponse).to.have.status(400);
-      expect(userResponse.body.message)
-        .to.be.equal('Token não encontrado ou informado');
+        .get("/api/users/2")
+        .set("authorization", token);
+
+      expect(userResponse).to.have.status(401);
+      expect(userResponse.body.message).to.be.equal("Acesso negado");
     });
-  })
+  });
+  describe("Falha ao fazer requisição sem token", () => {
+    it(
+      'Esperado status 400 e message "Token não ' +
+        'encontrado ou informado" ao fazer requisição sem token',
+      async () => {
+        const userResponse = await chai.request(server).get("/api/users/1");
+
+        expect(userResponse).to.have.status(400);
+        expect(userResponse.body.message).to.be.equal(
+          "Token não encontrado ou informado"
+        );
+      }
+    );
+  });
 });
