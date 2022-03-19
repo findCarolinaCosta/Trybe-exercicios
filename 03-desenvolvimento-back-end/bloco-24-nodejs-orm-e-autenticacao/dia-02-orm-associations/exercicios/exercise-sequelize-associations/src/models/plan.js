0e5947b12946c3fa4f24e2df4e346e986b9d6e21
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-03-19 19:25:47 -0300

    [24.2] Add model from Plans table

---
+++ 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations/exercicios/exercise-sequelize-associations/src/models/plan.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const Plan = sequelize.define('Plan', {
+    planId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    coverage: DataTypes.STRING,
+    price: DataTypes.DOUBLE
+  },
+  {
+    timestamps: false,
+    tableName: 'Plans',
+    underscored: true,
+  });
+  return Plan;
+};
