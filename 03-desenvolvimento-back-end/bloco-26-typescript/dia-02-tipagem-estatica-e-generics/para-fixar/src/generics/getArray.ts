feba0d6c11d51574d8977878ba30a3720da1be12
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-03-24 16:05:50 -0300

    [26.2] Add generic implementation in getArray function

--- 03-desenvolvimento-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/para-fixar/src/generics/getArray.ts
+++ 03-desenvolvimento-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/para-fixar/src/generics/getArray.ts
@@ -1,13 +1,14 @@
-function getArray(items : any[]) : any[] {
-  return new Array().concat(items);
+// para garantir valores de um determinado tipo é adicionada assim um generic
+function getArray<T>(items : T[]) : T[] {
+  return new Array<T>().concat(items);
 }
 
-let numberArray = getArray([5, 10, 15, 20]);
-let stringArray = getArray(["Cats", "Dogs", "Birds"]);
+let numberArray = getArray([5, 10, 15, 20]); // infere o tipo
+let stringArray = getArray(["Cats", "Dogs", "Birds"]);  // infere o tipo
 numberArray.push(25);
 stringArray.push("Rabbits");
-numberArray.push("isto não é um número");
-stringArray.push(30);
+// numberArray.push("isto não é um número"); // retorna erro com generic
+// stringArray.push(30); // retorna erro com generic
 console.log(numberArray);
 // Saída:  [5, 10, 15, 20, 25, "isto não é um número"]
 console.log(stringArray);
