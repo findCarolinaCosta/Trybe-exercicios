4981238e8bd872e0529768f23d6ee0c9cf5367ab
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-03-24 19:09:12 -0300

    [26.2] Add object instance of BookModel class in main function

--- 03-desenvolvimento-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/para-fixar/book-api/src/main.ts
+++ 03-desenvolvimento-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/para-fixar/book-api/src/main.ts
@@ -1,20 +1,33 @@
-import readline from 'readline-sync';
-import connection from './models/connection';
-import { ResultSetHeader } from 'mysql2/promise';
+// import readline from 'readline-sync';
+// import connection from './models/connection';
+// import { ResultSetHeader } from 'mysql2/promise';
+
+// const main = async () => {
+//   const title = readline.question('Digite o nome do livro: ');
+//   const price = readline.questionFloat('Digite o preço do livro: ');
+//   const author = readline.question('Digite o autor do livro: ');
+//   const isbn = readline.question('Digite o isbn do livro: ');
+
+//   const result = await connection.execute<ResultSetHeader>(
+//     'INSERT INTO books (title, price, author, isbn) VALUES (?, ?, ?, ?)',
+//     [title, price, author, isbn]
+//   );
+//   const [dataInserted] = result;
+//   const { insertId } = dataInserted;
+//   console.log(insertId);
+// }
+
+// main();
+
+
+import BookModel from "./models/Book";
+import connection  from "./models/connection";
 
 const main = async () => {
-  const title = readline.question('Digite o nome do livro: ');
-  const price = readline.questionFloat('Digite o preço do livro: ');
-  const author = readline.question('Digite o autor do livro: ');
-  const isbn = readline.question('Digite o isbn do livro: ');
+  const bookModel = new BookModel(connection);
 
-  const result = await connection.execute<ResultSetHeader>(
-    'INSERT INTO books (title, price, author, isbn) VALUES (?, ?, ?, ?)',
-    [title, price, author, isbn]
-  );
-  const [dataInserted] = result;
-  const { insertId } = dataInserted;
-  console.log(insertId);
+  const books = await bookModel.getAll();
+  console.log(books)
 }
 
 main();
