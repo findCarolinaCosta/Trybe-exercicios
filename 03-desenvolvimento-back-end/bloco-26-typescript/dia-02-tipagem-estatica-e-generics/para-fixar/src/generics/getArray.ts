243f7ebedfea32ba36fca8a32209dce0c5073d10
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-03-24 15:58:10 -0300

    [26.2] Adds example of getArray that accepts any type

---
+++ 03-desenvolvimento-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/para-fixar/src/generics/getArray.ts
@@ -0,0 +1,14 @@
+function getArray(items : any[]) : any[] {
+  return new Array().concat(items);
+}
+
+let numberArray = getArray([5, 10, 15, 20]);
+let stringArray = getArray(["Cats", "Dogs", "Birds"]);
+numberArray.push(25);
+stringArray.push("Rabbits");
+numberArray.push("isto não é um número");
+stringArray.push(30);
+console.log(numberArray);
+// Saída:  [5, 10, 15, 20, 25, "isto não é um número"]
+console.log(stringArray);
+// Saída: ["Cats", "Dogs", "Birds", "Rabbits", 30]
