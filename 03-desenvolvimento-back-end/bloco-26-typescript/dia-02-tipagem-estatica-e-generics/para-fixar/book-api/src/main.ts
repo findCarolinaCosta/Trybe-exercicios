cb86efaea3f8e5e31d0b1dc99387f0090d168681
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-03-24 19:25:36 -0300

    [26.2] Refactor initial function to create book with infos sent by terminal

--- 03-desenvolvimento-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/para-fixar/book-api/src/main.ts
+++ 03-desenvolvimento-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/para-fixar/book-api/src/main.ts
@@ -1,33 +1,29 @@
-// import readline from 'readline-sync';
-// import connection from './models/connection';
-// import { ResultSetHeader } from 'mysql2/promise';
+import readline from  'readline-sync';
 
-// const main = async () => {
-//   const title = readline.question('Digite o nome do livro: ');
-//   const price = readline.questionFloat('Digite o preço do livro: ');
-//   const author = readline.question('Digite o autor do livro: ');
-//   const isbn = readline.question('Digite o isbn do livro: ');
-
-//   const result = await connection.execute<ResultSetHeader>(
-//     'INSERT INTO books (title, price, author, isbn) VALUES (?, ?, ?, ?)',
-//     [title, price, author, isbn]
-//   );
-//   const [dataInserted] = result;
-//   const { insertId } = dataInserted;
-//   console.log(insertId);
-// }
-
-// main();
-
-
-import BookModel from "./models/Book";
+import BookModel, { Book } from "./models/Book";
 import connection  from "./models/connection";
 
-const main = async () => {
+const create = async () => {
+  const bookModel = new BookModel(connection);
+
+  const title = readline.question('Digite o título do livro: ');
+  const price = readline.questionFloat('Digite o preço do livro: ');
+  const author = readline.question('Digite o autor do livro: ');
+  const isbn = readline.question('Digite o isbn do livro: ');
+
+  const newBook: Book = { title , price , author , isbn };
+
+  const createdBook = await bookModel.create(newBook);
+  console.log(createdBook)
+}
+
+create();
+
+const getAll = async () => {
   const bookModel = new BookModel(connection);
 
   const books = await bookModel.getAll();
   console.log(books)
 }
 
-main();
+getAll();
