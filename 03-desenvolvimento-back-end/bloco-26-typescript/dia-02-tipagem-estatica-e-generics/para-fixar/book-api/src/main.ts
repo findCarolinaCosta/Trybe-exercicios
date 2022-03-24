ae70084b8c6da386fa2aec617a515b36d2e7333c
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-03-24 19:01:58 -0300

    [26.2] Add generics ResultSetHeader typing on db connection

--- 03-desenvolvimento-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/para-fixar/book-api/src/main.ts
+++ 03-desenvolvimento-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/para-fixar/book-api/src/main.ts
@@ -1,9 +1,20 @@
-import connection  from './models/connection';
+import readline from 'readline-sync';
+import connection from './models/connection';
+import { ResultSetHeader } from 'mysql2/promise';
 
 const main = async () => {
-  const result = await connection.execute('SELECT * FROM books');
-  const [rows] = result;
-  console.log(rows);
+  const title = readline.question('Digite o nome do livro: ');
+  const price = readline.questionFloat('Digite o preço do livro: ');
+  const author = readline.question('Digite o autor do livro: ');
+  const isbn = readline.question('Digite o isbn do livro: ');
+
+  const result = await connection.execute<ResultSetHeader>(
+    'INSERT INTO books (title, price, author, isbn) VALUES (?, ?, ?, ?)',
+    [title, price, author, isbn]
+  );
+  const [dataInserted] = result;
+  const { insertId } = dataInserted;
+  console.log(insertId);
 }
 
 main();
