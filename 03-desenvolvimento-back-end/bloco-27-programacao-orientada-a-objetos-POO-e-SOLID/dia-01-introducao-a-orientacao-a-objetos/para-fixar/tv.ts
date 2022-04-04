bda475db1693a2bf9a65f8c3b2def0d390d15c6c
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-04-04 19:03:27 -0300

    [27.1] Adds encapsulation syntax fixing exercise

--- 03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-POO-e-SOLID/dia-01-introducao-a-orientacao-a-objetos/para-fixar/tv.ts
+++ 03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-POO-e-SOLID/dia-01-introducao-a-orientacao-a-objetos/para-fixar/tv.ts
@@ -1,23 +1,41 @@
 //1. Crie uma classe chamada Tv, com os atributos: brand, size, resolution, connections, connectedTo
 class Tv {
-  brand: string;
-  size: number;
-  resolution: string;
-  connections: string[];
-  connectedTo: string; //conexão atual Este atributo não precisa ser inicializado no construtor 
+  // 5. Altere a visibilidade dos atributos definidos na classe Tv para private 
+  private _brand: string;
+  private _size: number;
+  private _resolution: string;
+  private _connections: string[];
+  private _connectedTo: string;
 
   constructor(brand: string, size: number, resolution: string, connections: string[]) {
-    this.brand = brand;
-    this.size = size;
-    this.resolution = resolution;
-    this.connections = connections;
+    this._brand = brand;
+    this._size = size;
+    this._resolution = resolution;
+    this._connections = connections;
   }
   //2. Dentro da classe Tv, crie o método turnOn , que imprimirá os atributos inicializados no construtor.
   turnOn():void {
-    console.log(`TV ${this.brand}, ${this.size}", resolution: ${this.resolution} \navailable connections: ${this.connections}`);
+    console.log(`TV ${this._brand}, ${this._size}", resolution: ${this._resolution} \navailable connections: ${this._connections}`);
+  }
+// 6. Crie um método getter e um setter para o atributo _connectedTo , da classe Tv.
+  get connectedTo(): string {
+    return this._connectedTo;
+  }
+
+  set connectedTo(value: string) {
+    if (this._connections.includes(value)) {
+      this._connectedTo = value;
+      console.log(this._connectedTo);
+    } else {
+      console.log('Sorry, connection unavailable!');
+    }
   }
 }
 
 //3. Instancie um objeto a partir da classe Tv, chame o método turnOn para imprimir seus atributos.
-const tv1 = new Tv('SAMSUNG', 32, '4K', ['HDMI', 'Wifi']);
+const tv1 = new Tv('SAMSUNG', 32, '4K', ['HDMI', 'Wi-Fi']);
 tv1.turnOn();
+
+// 7. Defina um valor para o atributo _connectedTo por meio do método setter criado e imprima seu valor.
+tv1.connectedTo = 'Wi-Fi';
+console.log('Connected to: ', tv1.connectedTo);
