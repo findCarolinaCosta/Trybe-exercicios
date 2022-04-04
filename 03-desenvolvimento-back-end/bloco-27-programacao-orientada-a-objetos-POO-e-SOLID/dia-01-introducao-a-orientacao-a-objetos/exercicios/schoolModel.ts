6fa812438a324192d064fcb30d57652bf6f94db6
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-04-04 20:09:09 -0300

    [27.1] Add method of summing and averaging grades in student class

--- 03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-POO-e-SOLID/dia-01-introducao-a-orientacao-a-objetos/exercicios/schoolModel.ts
+++ 03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-POO-e-SOLID/dia-01-introducao-a-orientacao-a-objetos/exercicios/schoolModel.ts
@@ -1,4 +1,3 @@
-
 class Student {
   private _registration: string;
   private _name: string = String();
@@ -6,62 +5,84 @@ class Student {
   private _worksGrades: number[];
 
   constructor(enrollment: string, name: string) {
-      this._registration = enrollment;
-      this.name = name;
-      this._examsGrades = [];
-      this._worksGrades = [];
+    this._registration = enrollment;
+    this.name = name;
+    this._examsGrades = [];
+    this._worksGrades = [];
   }
 
   get enrollment(): string {
-      return this._registration;
+    return this._registration;
   }
 
   set enrollment(value: string) {
-      this._registration = value;
+    this._registration = value;
   }
 
   get name(): string {
-      return this._name;
+    return this._name;
   }
 
   set name(value: string) {
-      if (value.length < 3) {
-          throw new Error('O nome deve conter no mínimo 3 caracteres.');
-      }
+    if (value.length < 3) {
+      throw new Error("O nome deve conter no mínimo 3 caracteres.");
+    }
 
-      this._name = value;
+    this._name = value;
   }
 
   get examsGrades(): number[] {
-      return this._examsGrades;
+    return this._examsGrades;
   }
 
   set examsGrades(value: number[]) {
-      if (value.length > 4) {
-          throw new Error('A pessoa estudante só pode possuir 4 notas de provas.');
-      }
+    if (value.length > 4) {
+      throw new Error("A pessoa estudante só pode possuir 4 notas de provas.");
+    }
 
-      this._examsGrades = value;
+    this._examsGrades = value;
   }
 
   get worksGrades(): number[] {
-      return this._worksGrades;
+    return this._worksGrades;
   }
 
   set worksGrades(value: number[]) {
-      if (value.length > 2) {
-          throw new Error('A pessoa estudante só pode possuir 2 notas de trabalhos.');
-      }
+    if (value.length > 2) {
+      throw new Error(
+        "A pessoa estudante só pode possuir 2 notas de trabalhos."
+      );
+    }
 
-      this._worksGrades = value;
+    this._worksGrades = value;
   }
 
-}
+  sumGrades(): number {
+    return [...this.examsGrades, ...this.worksGrades].reduce(
+      (previousNote, note) => {
+        note += previousNote;
 
+        return note;
+      },
+      0
+    );
+  }
+
+  averageGrades(): number {
+    const sumNotes = this.sumGrades();
+    const divider = this.examsGrades.length + this.worksGrades.length;
+
+    return Math.round(sumNotes / divider);
+  }
+}
 
 // Para testar!
 
 const studentOne = new Student('20140120', 'Aurora Rosa');
-const studentTwo = new Student('20001212', 'Joaquim Rosa');
 
-console.log({Students: [studentOne, studentTwo]});
+studentOne.examsGrades = [25, 20, 23, 23];
+studentOne.worksGrades = [45, 45];
+
+console.log(studentOne);
+console.log('Soma de todas as notas: ', studentOne.sumGrades());
+console.log('Média de todas as notas: ', studentOne.averageGrades());
