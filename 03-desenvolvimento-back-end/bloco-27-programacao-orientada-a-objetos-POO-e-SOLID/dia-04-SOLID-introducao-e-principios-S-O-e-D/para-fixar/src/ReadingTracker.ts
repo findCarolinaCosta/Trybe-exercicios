857b0c59cfa2c7b4b892bdc8ca861f103d467c2f
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-04-09 19:57:13 -0300

    [27.4] Adds exercise solution for fixation using SRP

--- 03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-POO-e-SOLID/dia-04-SOLID-introducao-e-principios-S-O-e-D/para-fixar/src/ReadingTracker.ts
+++ 03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-POO-e-SOLID/dia-04-SOLID-introducao-e-principios-S-O-e-D/para-fixar/src/ReadingTracker.ts
@@ -1,50 +1,61 @@
-// 1 - Observe o código abaixo e faça as alterações necessárias para que ele passe a respeitar o Princípio da Responsabilidade Única (SRP) .
-
-type Book = {
-  book: string;
-  author: string;
-  genre: string;
-}
+import progressNotification from "./notifications";
 
 class ReadingTracker {
   private readingGoal: number;
   private booksRead: number;
-  private wishlist: Book[];
 
   constructor(readingGoal: number) {
     this.readingGoal = readingGoal;
     this.booksRead = 0;
-    this.wishlist = [];
   }
 
   trackReadings(readsCount: number) {
     this.booksRead += readsCount;
     if (this.booksRead >= this.readingGoal) {
-      this.progressNotification(
+      progressNotification(
         "Congratulations! You've reached your reading goal!"
       );
       return;
     }
-    this.progressNotification(
-      "There are still some books to go!"
-    );
-  }
-
-  addToWishList(book: Book): void {
-    this.wishlist.push(book);
-  }
-
-  showWishlist(): void {
-    console.log(this.wishlist);
-  }
-
-  progressNotification(message: string): void {
-    console.log(message);
+    progressNotification("There are still some books to go!");
   }
 }
 
 const readTracker = new ReadingTracker(20);
-readTracker.addToWishList({'The Road', 'Cormac McCarthy', 'Dystopia'});
-readTracker.showWishlist();
 readTracker.trackReadings(12);
 readTracker.trackReadings(9);
+
+// BooksWishlist.ts
+type Book = {
+  book: string;
+  author: string;
+  genre: string;
+};
+
+class BooksWishlist {
+  private wishlist: Book[];
+  constructor(book: Book) {
+    this.wishlist = [];
+    this.wishlist.push(book);
+  }
+
+  addToWishList(newBook: Book): void {
+    this.wishlist.push(newBook);
+  }
+
+  showWishlist(): void {
+    return console.log(this.wishlist);
+  }
+}
+
+const wishlist = new BooksWishlist({
+  book: "The Road",
+  author: "Cormac McCarthy",
+  genre: "Dystopia",
+});
+wishlist.addToWishList({
+  book: "Misery",
+  author: "Stephen King",
+  genre: "Thriller",
+});
+wishlist.showWishlist();
