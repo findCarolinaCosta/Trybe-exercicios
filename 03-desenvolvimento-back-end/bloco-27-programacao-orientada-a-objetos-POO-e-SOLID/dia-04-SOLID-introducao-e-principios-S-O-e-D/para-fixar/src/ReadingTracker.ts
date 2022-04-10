7dcc2e181386ba58fa88ad179bef32cd2d343f34
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-04-10 00:28:20 -0300

    [27.4] Implements dependency inversion principle

--- 03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-POO-e-SOLID/dia-04-SOLID-introducao-e-principios-S-O-e-D/para-fixar/src/ReadingTracker.ts
+++ 03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-POO-e-SOLID/dia-04-SOLID-introducao-e-principios-S-O-e-D/para-fixar/src/ReadingTracker.ts
@@ -1,25 +1,56 @@
-import progressNotification from './notifications';
+interface INotificator {
+  sendNotification(message: string): void;
+}
 
-class ReadingTracker {
+class ConsoleNotification implements INotificator {
+  sendNotification(message: string) {
+    console.log(`Here we go again! - ${message}`);
+  }
+}
+
+class EmailNotification implements INotificator {
+  private email: string;
+  constructor(email: string) {
+    this.email = email;
+  }
+  sendNotification(message: string) {
+    console.log(
+      `Here should go the implementation to send notification to the email: ${this.email} - ${message}`
+    );
+  }
+}
+
+class PhoneNotification implements INotificator {
+  private phone: number;
+  constructor(phone: number) {
+    this.phone = phone;
+  }
+  sendNotification(message: string) {
+    console.log(
+      `Here should go the implementation to send notification to the phone ${this.phone} - ${message}`
+    );
+  }
+}
+
+export class ReadingTracker {
   private readingGoal: number;
-
   private booksRead: number;
-
-  constructor(readingGoal: number) {
+  
+  constructor(readingGoal: number, public notificator: INotificator = new ConsoleNotification()) {
     this.readingGoal = readingGoal;
     this.booksRead = 0;
   }
 
+
   trackReadings(readsCount: number) {
     this.booksRead += readsCount;
     if (this.booksRead >= this.readingGoal) {
-      progressNotification(
-        'Congratulations! You\'ve reached your reading goal!',
-        'Console',
-      );
+      this.notificator.sendNotification(
+        "Congratulations! You've reached your reading goal!"
+      )
       return;
     }
-    progressNotification('There are still some books to go!', 'Console');
+    this.notificator.sendNotification("There are still some books to go!");
   }
 }
 
