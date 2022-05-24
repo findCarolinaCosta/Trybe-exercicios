137d527a01310ec4a8de796f41a16ceac00248d4
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-05-23 21:06:08 -0300

    [30.1] add bookModel method to update book by id

--- 03-desenvolvimento-back-end/bloco-30-mongodb-com-node.js-e-poo/dia-01-mongodb-e-arquitetura-msc/para-fixar/Bookshop/src/models/BookModel.ts
+++ 03-desenvolvimento-back-end/bloco-30-mongodb-com-node.js-e-poo/dia-01-mongodb-e-arquitetura-msc/para-fixar/Bookshop/src/models/BookModel.ts
@@ -18,4 +18,13 @@ export class BookModel {
     const book = await this.bookModel.findOne({ _id: id });
     return book;
   }
+
+  public async editBook(id: string, bookData: object): Promise<IBook | null> {
+    const book = await this.bookModel.findOneAndUpdate(
+      { _id: id },
+      { ...bookData },
+      { new: true } //
+    );
+    return book;
+  }
 }
